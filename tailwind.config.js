cbafc283e5a1ab6b8e552869a7f2f977a47355c6
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-04-22 17:04:36 +0200

    Added fonts to config.

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      // colors: {
+      //   primary: "#161622",
+      //   secondary: {
+      //     DEFAULT: "#FF9C01",
+      //     100: "#FF9001",
+      //     200: "#FF8E01",
+      //   },
+      //   black: {
+      //     DEFAULT: "#000",
+      //     100: "#1E1E2D",
+      //     200: "#232533",
+      //   },
+      //   gray: {
+      //     100: "#CDCDE0",
+      //   },
+      // },
+      fontFamily: {
+        poppinsThin: ["Poppins-Thin", "sans-serif"],
+        poppingExtraLight: ["Poppins-ExtraLight", "sans-serif"],
+        poppinsLight: ["Poppins-Light", "sans-serif"],
+        poppinsRegular: ["Poppins-Regular", "sans-serif"],
+        poppinsMedium: ["Poppins-Medium", "sans-serif"],
+        poppinsSemiBold: ["Poppins-SemiBold", "sans-serif"],
+        poppinsBold: ["Poppins-Bold", "sans-serif"],
+        poppinsExtrabold: ["Poppins-ExtraBold", "sans-serif"],
+        poppinsBlack: ["Poppins-Black", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
