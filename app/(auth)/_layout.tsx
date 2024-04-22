87eba0ca5c237f2904fd80e6bab3facb66266efc
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-04-22 17:05:26 +0200

    Added layout for auth pages.

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,25 @@
+import { View, Text } from "react-native";
+import { Stack } from "expo-router";
+import React from "react";
+
+export default function AuthLayout() {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="signIn"
+        options={{
+          title: "Zaloguj się",
+          headerShown: false,
+        }}
+      />
+
+      <Stack.Screen
+        name="signUp"
+        options={{
+          title: "Zaloguj się",
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
+}
