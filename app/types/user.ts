0f393f17d007208c6012b9373937dc887bd61f5e
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-04-22 17:08:06 +0200

    Created type for user.

---
+++ app/types/user.ts
@@ -0,0 +1,12 @@
+export type User = {
+  readonly $collectionId: string;
+  readonly $createdAt: string;
+  readonly $databaseId: string;
+  readonly $id: string;
+  readonly $permissions: string[];
+  readonly $updatedAt: string;
+  readonly accountId: string;
+  readonly avatar: string;
+  readonly email: string;
+  readonly username: string;
+};
