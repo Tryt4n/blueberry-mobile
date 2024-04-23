ffab61e67921481fbe3715f595885bba38793a82
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-04-23 14:32:01 +0200

    Added redirection based on whether the user is logged in or not.

--- app/index.tsx
+++ app/index.tsx
@@ -1,54 +1,14 @@
-import { Button, Image, ScrollView, Text } from "react-native";
-import { StatusBar } from "expo-status-bar";
-import React, { useEffect } from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { useGlobalContext } from "./hooks/useGlobalContext";
-import { Link, router } from "expo-router";
-import { signOut } from "@/api/auth/users";
-import CustomButton from "@/components/CustomButton";
+import React from "react";
+import { Redirect } from "expo-router";
+import { useGlobalContext } from "../hooks/useGlobalContext";
+import LoadingSpinner from "@/components/LoadingSpinner";
 
 export default function App() {
-  const { isLoading, isLoggedIn, user } = useGlobalContext();
+  const { isLoading, isLoggedIn } = useGlobalContext();
 
-  if (isLoggedIn) return null;
+  if (isLoading) return <LoadingSpinner color="rgb(59 130 246)" />;
 
-  // if (isLoading === false && isLoggedIn === false) return router.replace("/signIn");
+  if (!isLoading && isLoggedIn) return <Redirect href="/fertilizers" />;
 
-  // if (!isLoading && isLoggedIn) return router.replace("/signIn");
-  // if (!isLoading && !isLoggedIn) return router.replace("/signIn");
-
-  if (isLoggedIn !== false) router.push("/fertilizers");
-
-  return (
-    <SafeAreaView>
-      <ScrollView>
-        {/* <StatusBar
-          backgroundColor="#161622"
-          style="light"
-        /> */}
-
-        <Text>Blueberry App</Text>
-
-        <Link
-          href={"/fertilizers"}
-          className="p-4 bg-slate-500"
-        >
-          Go to fertilizers
-        </Link>
-        <Link
-          href={"/(drawer)/(tabs)/fertilizers"}
-          className="p-4 bg-red-300"
-        >
-          Go to fertilizers
-        </Link>
-
-        <CustomButton
-          text="Wyloguj się"
-          activeOpacity={0.7}
-          // disabled={isLoading}
-          onPress={signOut}
-        />
-      </ScrollView>
-    </SafeAreaView>
-  );
+  if (!isLoading && !isLoggedIn) return <Redirect href="/signIn" />;
 }
