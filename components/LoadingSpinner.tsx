ea8ca3aa85e07c46bbaa6553f214e81cda1b236e
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-04-23 14:36:11 +0200

    Added `prop` to change spinner color.

--- components/LoadingSpinner.tsx
+++ components/LoadingSpinner.tsx
@@ -1,13 +1,16 @@
 import { ActivityIndicator, View } from "react-native";
 import React from "react";
 
-export default function LoadingSpinner() {
+export default function LoadingSpinner({ color }: { color?: string }) {
   return (
     <View
       className="h-full flex justify-center"
       aria-label="Loading..."
     >
-      <ActivityIndicator size="large" />
+      <ActivityIndicator
+        size="large"
+        color={color}
+      />
     </View>
   );
 }
