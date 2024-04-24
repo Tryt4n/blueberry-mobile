8b624a38c7b3738fd7484a7d7b154976dec4bb2a
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-04-24 15:10:52 +0200

    Added `zod` validation for `createUser` function.

--- api/auth/users.ts
+++ api/auth/users.ts
@@ -1,6 +1,8 @@
 import { account, appwriteConfig, avatars, databases } from "@/lib/appwrite";
 import { ID, Query } from "react-native-appwrite";
 import type { User } from "@/types/user";
+import { SignupSchema } from "@/lib/zod/auth";
+import type { ErrorKeys } from "@/types/Errors";
 
 export async function signIn(email: string, password: string) {
   try {
@@ -22,29 +24,86 @@ export async function signOut() {
   }
 }
 
-export async function createUser(email: string, password: string, username: string) {
+//!
+// export async function createUser(email: string, password: string, username: string) {
+//   try {
+//     const newAccount = await account.create(ID.unique(), email, password, username);
+
+//     if (!newAccount) throw Error("Failed to create account");
+
+//     const avatarUrl = avatars.getInitials(username);
+
+//     await signIn(email, password);
+
+//     const newUser = await databases.createDocument(
+//       appwriteConfig.databaseId,
+//       appwriteConfig.userCollectionId,
+//       ID.unique(),
+//       { accountId: newAccount.$id, email, username, avatar: avatarUrl }
+//     );
+
+//     return newUser as User;
+//   } catch (error: any) {
+//     console.error(error);
+//     throw new Error(error);
+//   }
+// }
+
+export async function createUser(
+  email: string,
+  password: string,
+  username: string,
+  passwordConfirmation: string
+) {
+  let customErrors: Record<ErrorKeys, string[]> = {
+    email: [],
+    username: [],
+    password: [],
+    passwordConfirmation: [],
+    alert: [],
+  };
+
   try {
-    const newAccount = await account.create(ID.unique(), email, password, username);
+    const results = SignupSchema.safeParse({ email, password, username, passwordConfirmation });
 
-    if (!newAccount) throw Error("Failed to create account");
+    if (!results.success) {
+      results.error.issues.forEach((issue) => {
+        const field = issue.path[0] as ErrorKeys;
+        if (field in customErrors) {
+          customErrors[field].push(issue.message);
+        }
+      });
 
-    const avatarUrl = avatars.getInitials(username);
+      return { user: null, errors: customErrors };
+    } else {
+      const newAccount = await account.create(ID.unique(), email, password, username);
 
-    await signIn(email, password);
+      if (!newAccount) throw Error("Failed to create account");
 
-    const newUser = await databases.createDocument(
-      appwriteConfig.databaseId,
-      appwriteConfig.userCollectionId,
-      ID.unique(),
-      { accountId: newAccount.$id, email, username, avatar: avatarUrl }
-    );
+      const avatarUrl = avatars.getInitials(username);
 
-    return newUser as User;
+      await signIn(email, password);
+
+      const newUser = await databases.createDocument(
+        appwriteConfig.databaseId,
+        appwriteConfig.userCollectionId,
+        ID.unique(),
+        { accountId: newAccount.$id, email, username, avatar: avatarUrl }
+      );
+
+      return { user: newUser as User, errors: customErrors };
+    }
   } catch (error: any) {
-    console.error(error);
-    throw new Error(error);
+    return {
+      user: null,
+      errors: {
+        ...customErrors,
+        alert: ["Użytkownik z takim samym emailem już istnieje"],
+      },
+    };
   }
 }
+//!
 
 export async function getCurrentUser() {
   try {
