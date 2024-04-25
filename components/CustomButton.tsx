b05567b127519983de6a3b1f2910308ddceff2ef
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-04-25 14:17:40 +0200

    Edited button to show loading spinner if loading props are passed.

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,16 +1,20 @@
-import { Text, TouchableOpacity } from "react-native";
+import { ActivityIndicator, Text, TouchableOpacity } from "react-native";
 import React, { type ComponentPropsWithoutRef } from "react";
 
 type CustomButtonProps = {
   text: string;
   containerStyles?: string;
   textStyles?: string;
+  loading?: boolean;
+  loadingColor?: string;
 } & Omit<ComponentPropsWithoutRef<typeof TouchableOpacity>, "className">;
 
 export default function CustomButton({
   text,
   containerStyles,
   textStyles,
+  loading,
+  loadingColor = "white",
   ...props
 }: CustomButtonProps) {
   return (
@@ -21,13 +25,21 @@ export default function CustomButton({
       {...props}
       activeOpacity={0.7}
     >
-      <Text
-        className={`text-white font-poppinsSemiBold text-base p-4${
-          textStyles ? ` ${textStyles}` : ""
-        }`}
-      >
-        {text}
-      </Text>
+      {!loading ? (
+        <Text
+          className={`text-white font-poppinsSemiBold text-base p-4${
+            textStyles ? ` ${textStyles}` : ""
+          }`}
+        >
+          {text}
+        </Text>
+      ) : (
+        <ActivityIndicator
+          className="p-3 text-base font-poppinsSemiBold"
+          size="large"
+          color={loadingColor}
+        />
+      )}
     </TouchableOpacity>
   );
 }
