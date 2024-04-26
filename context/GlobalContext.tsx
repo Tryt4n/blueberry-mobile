ed989f962d048d0598d5c0eacf8d9fb8f6c86b75
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-04-26 16:07:54 +0200

    Added `authProvider` state.

--- context/GlobalContext.tsx
+++ context/GlobalContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
-import { getCurrentUser } from "@/api/auth/users";
+import { getCurrentUser } from "@/api/auth/appwrite";
 import type { User } from "../types/user";
+import type { AuthProviders } from "@/types/authProviders";
 
 type GlobalContextValues = {
   isLoggedIn: boolean;
@@ -8,6 +9,8 @@ type GlobalContextValues = {
   user: User | null;
   setUser: (value: User | null) => void;
   isLoading: boolean;
+  authProvider: AuthProviders;
+  setAuthProvider: (value: AuthProviders) => void;
 };
 
 export const GlobalContext = createContext<GlobalContextValues | null>(null);
@@ -16,6 +19,7 @@ export default function GlobalContextProvider({ children }: { children: React.Re
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [user, setUser] = useState<User | null>(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [authProvider, setAuthProvider] = useState<AuthProviders>("appwrite");
 
   useEffect(() => {
     getCurrentUser()
@@ -42,6 +46,8 @@ export default function GlobalContextProvider({ children }: { children: React.Re
     user,
     setUser,
     isLoading,
+    authProvider,
+    setAuthProvider,
   };
 
   return <GlobalContext.Provider value={contextValues}>{children}</GlobalContext.Provider>;
