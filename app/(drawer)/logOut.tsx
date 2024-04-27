554e2c97a3c86c2dc59a4123509236fee33f02ff
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-04-27 15:50:18 +0200

    Layout changes.

--- app/(drawer)/logOut.tsx
+++ app/(drawer)/logOut.tsx
@@ -1,12 +1,10 @@
-import { View, Text, Alert } from "react-native";
+import { View, Text, Alert, ScrollView } from "react-native";
 import React, { useState } from "react";
 import { router } from "expo-router";
-import { ScrollView } from "react-native-gesture-handler";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { useGlobalContext } from "../../hooks/useGlobalContext";
 import { signOut } from "@/api/auth/appwrite";
-import CustomButton from "@/components/CustomButton";
 import { signOutWithGoogle } from "@/api/auth/google";
+import CustomButton from "@/components/CustomButton";
 
 export default function LogOutPage() {
   const { isLoggedIn, setIsLoggedIn, setUser, authProvider } = useGlobalContext();
@@ -25,7 +23,6 @@ export default function LogOutPage() {
 
       router.replace("/signIn");
     } catch (error) {
-      console.error(error);
       Alert.alert("Błąd", "Nie udało się wylogować. Spróbuj ponownie.");
     } finally {
       setIsSubmitting(false);
@@ -33,32 +30,34 @@ export default function LogOutPage() {
   }
 
   return (
-    <SafeAreaView>
-      <ScrollView
-        contentContainerStyle={{ alignItems: "center", justifyContent: "center", height: "100%" }}
-      >
-        <View>
-          <Text className="font-poppinsSemiBold text-2xl text-center mb-4">Wylogować się?</Text>
-          <View className="flex flex-row justify-between gap-4">
-            <CustomButton
-              text="Tak"
-              onPress={logOut}
-              disabled={isSubmitting}
-              containerStyles="bg-transparent border-2 border-blue-500 px-8"
-              textStyles="text-black"
-              loading={isSubmitting}
-              loadingColor="rgb(59 130 246)"
-            />
+    <ScrollView
+      contentContainerStyle={{
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100%",
+      }}
+    >
+      <View>
+        <Text className="font-poppinsSemiBold text-2xl text-center mb-4">Wylogować się?</Text>
+        <View className="flex flex-row justify-between gap-4">
+          <CustomButton
+            text="Tak"
+            onPress={logOut}
+            disabled={isSubmitting}
+            containerStyles="bg-transparent border-2 border-blue-500 px-8"
+            textStyles="text-black"
+            loading={isSubmitting}
+            loadingColor="rgb(59 130 246)"
+          />
 
-            <CustomButton
-              text="Nie"
-              containerStyles="px-8"
-              disabled={isSubmitting}
-              onPress={() => router.back()}
-            />
-          </View>
+          <CustomButton
+            text="Nie"
+            containerStyles="px-8"
+            disabled={isSubmitting}
+            onPress={() => router.back()}
+          />
         </View>
-      </ScrollView>
-    </SafeAreaView>
+      </View>
+    </ScrollView>
   );
 }
