b11c17af6e24d3751485d72faf2ef61acd99bb7e
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-04-28 13:58:36 +0200

    Edited `createOrder` function to include teh buyer.

--- api/appwrite/orders.ts
+++ api/appwrite/orders.ts
@@ -2,6 +2,7 @@ import { appwriteConfig, databases } from "@/lib/appwrite";
 import { ID } from "react-native-appwrite";
 import type { User } from "@/types/user";
 import type { Order } from "@/types/orders";
+import type { Buyer } from "@/types/buyers";
 
 export async function getOrders() {
   try {
@@ -10,13 +11,18 @@ export async function getOrders() {
       appwriteConfig.ordersCollectionId
     );
 
-    return orders.documents as Order[];
+    return orders.documents;
   } catch (error: any) {
     throw new Error(error);
   }
 }
 
-export async function createOrder(userId: User["$id"], quantity: number, additionalInfo?: string) {
+export async function createOrder(
+  userId: User["$id"],
+  buyerId: Buyer["$id"],
+  quantity: number,
+  additionalInfo?: string
+) {
   try {
     const order = await databases.createDocument(
       appwriteConfig.databaseId,
@@ -26,6 +32,7 @@ export async function createOrder(userId: User["$id"], quantity: number, additio
         user: userId,
         quantity: quantity,
         additionalInfo: additionalInfo ? additionalInfo : null,
+        buyer: buyerId,
       }
     );
 
