bd7d23453266db1d7050ff2cee8ded1e165bfce9
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-04-28 14:00:20 +0200

    Changed formatting.

--- app/(drawer)/(tabs)/_layout.tsx
+++ app/(drawer)/(tabs)/_layout.tsx
@@ -4,76 +4,72 @@ import TabIcon from "@/components/TabIcon";
 
 export default function TabsLayout() {
   return (
-    <>
-      <Tabs
-        screenOptions={{
-          headerShown: false,
-          tabBarShowLabel: false,
-          tabBarStyle: {
-            height: 84,
-          },
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: { height: 84 },
+      }}
+    >
+      <Tabs.Screen
+        name="orders"
+        options={{
+          title: "Zamówienia",
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              icon="cart"
+              color={color}
+              name="Zamówienia"
+              focused={focused}
+            />
+          ),
         }}
-      >
-        <Tabs.Screen
-          name="orders"
-          options={{
-            title: "Zamówienia",
-            tabBarIcon: ({ color, focused }) => (
-              <TabIcon
-                icon="cart"
-                color={color}
-                name="Zamówienia"
-                focused={focused}
-              />
-            ),
-          }}
-        />
+      />
 
-        <Tabs.Screen
-          name="(fertigation)"
-          options={{
-            title: "Fertygacja",
-            tabBarIcon: ({ color, focused }) => (
-              <TabIcon
-                icon="leaf"
-                color={color}
-                name="Fertygacja"
-                focused={focused}
-              />
-            ),
-          }}
-        />
+      <Tabs.Screen
+        name="(fertigation)"
+        options={{
+          title: "Fertygacja",
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              icon="leaf"
+              color={color}
+              name="Fertygacja"
+              focused={focused}
+            />
+          ),
+        }}
+      />
 
-        <Tabs.Screen
-          name="weather"
-          options={{
-            title: "Pogoda",
-            tabBarIcon: ({ color, focused }) => (
-              <TabIcon
-                icon="sunny"
-                color={color}
-                name="Pogoda"
-                focused={focused}
-              />
-            ),
-          }}
-        />
+      <Tabs.Screen
+        name="weather"
+        options={{
+          title: "Pogoda",
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              icon="sunny"
+              color={color}
+              name="Pogoda"
+              focused={focused}
+            />
+          ),
+        }}
+      />
 
-        <Tabs.Screen
-          name="management"
-          options={{
-            title: "Zarządzanie",
-            tabBarIcon: ({ color, focused }) => (
-              <TabIcon
-                icon="wallet"
-                color={color}
-                name="Zarządzanie"
-                focused={focused}
-              />
-            ),
-          }}
-        />
-      </Tabs>
-    </>
+      <Tabs.Screen
+        name="management"
+        options={{
+          title: "Zarządzanie",
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              icon="wallet"
+              color={color}
+              name="Zarządzanie"
+              focused={focused}
+            />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
