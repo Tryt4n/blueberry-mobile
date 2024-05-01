5685c937247c5abc4ccb004fccc5cb8d4016682e
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-05-01 12:41:39 +0200

    Extracted order card related code to its own component.

--- app/(drawer)/(tabs)/orders.tsx
+++ app/(drawer)/(tabs)/orders.tsx
@@ -6,20 +6,25 @@ import { getOrders } from "@/api/appwrite/orders";
 import { useGlobalContext } from "@/hooks/useGlobalContext";
 import { useBottomSheetContext } from "@/hooks/useBottomSheetContext";
 import { useAppwrite } from "@/hooks/useAppwrite";
-import { formatDistanceToNow } from "date-fns";
-import { pl } from "date-fns/locale";
+import OrderCard from "@/components/OrderCard";
+import CustomButton from "@/components/CustomButton";
 
 export default function TabOrders() {
   const { user } = useGlobalContext();
-  const { handleOpenPress } = useBottomSheetContext();
+  const { handleOpenBottomSheet } = useBottomSheetContext();
+  const currentPrice = 13;
   const {
     data: orders,
     isLoading,
     refetchData,
-  } = useAppwrite(getOrders, {
-    title: "Błąd",
-    message: "Nie udało się pobrać zamówień. Spróbuj odświeżyć stronę.",
-  });
+  } = useAppwrite(
+    getOrders,
+    (user && user.role !== "admin") || (user && user.role !== "moderator") ? [user.$id] : [],
+    {
+      title: "Błąd",
+      message: "Nie udało się pobrać zamówień. Spróbuj odświeżyć stronę.",
+    }
+  );
   const [refreshing, setRefreshing] = useState(false);
 
   async function refetchOrders() {
@@ -32,74 +37,74 @@ export default function TabOrders() {
 
   return (
     <View className="h-full w-[90%] mx-auto">
-      <View className="mt-8 flex flex-row justify-between pb-2">
-        <Text className="font-poppinsBold text-3xl">Zamówienia</Text>
+      {user && (user.role === "admin" || user.role === "moderator" || user.role === "seller") && (
+        <>
+          <View className="mt-8 flex flex-row justify-between pb-2">
+            <Text className="font-poppinsBold text-3xl">Zamówienia</Text>
 
-        {user && (user.role === "admin" || user.role === "moderator" || user.role === "seller") && (
-          <TouchableOpacity
-            disabled={isLoading}
-            className="-translate-y-[12px]"
-            onPress={handleOpenPress}
-            aria-label="Dodaj nowe zamówienie"
-          >
-            <Ionicons
-              name="add-circle"
-              size={48}
-              color="rgb(59 130 246)"
-            />
-          </TouchableOpacity>
-        )}
-      </View>
-      <Text className="font-poppinsRegular text-base">
-        Cena: <Text className="font-poppinsMedium text-xl">13 zł</Text>
-      </Text>
+            <TouchableOpacity
+              disabled={isLoading}
+              className="-translate-y-[12px]"
+              onPress={handleOpenBottomSheet}
+              aria-label="Dodaj nowe zamówienie"
+            >
+              <Ionicons
+                name="add-circle"
+                size={48}
+                color="rgb(59 130 246)"
+              />
+            </TouchableOpacity>
+          </View>
 
-      <View className="my-4 h-[75%]">
-        {isLoading ? (
-          <ActivityIndicator
-            size="large"
-            color="rgb(59 130 246)"
-            className="my-8"
-          />
-        ) : (
-          <>
-            {orders && orders.length > 0 && (
-              <FlatList
-                data={orders}
-                keyExtractor={(item) => item.$id}
-                renderItem={({ item }) => (
-                  <View>
-                    <Text>Ilość: {item.quantity}</Text>
-                    <Text>Dla: {item.buyer.buyerName}</Text>
-                    <Text>Ukończone: {item.completed ? "Tak" : "Nie"}</Text>
-                    {item.additionalInfo && (
-                      <Text>Informacje dodatkowe: {item.additionalInfo}</Text>
-                    )}
-                    <View>
-                      <Text>
-                        Dodano przez <Text>{item.user.username}</Text>
-                      </Text>
-                      <Text>
-                        {formatDistanceToNow(item.$createdAt, {
-                          addSuffix: true,
-                          locale: pl,
-                        })}
-                      </Text>
-                    </View>
-                  </View>
-                )}
-                refreshControl={
-                  <RefreshControl
-                    refreshing={refreshing}
-                    onRefresh={refetchOrders}
-                  />
-                }
-                className="p-2 border rounded-lg"
+          <View className="flex-row items-center gap-4 mb-4">
+            <Text className="font-poppinsRegular text-base">
+              Cena: <Text className="font-poppinsMedium text-xl">{currentPrice} zł</Text>
+            </Text>
+            {(user.role === "admin" || user.role === "moderator") && (
+              <CustomButton
+                text="Zmień cenę"
+                containerStyles=""
+                textStyles="text-xs p-3"
               />
             )}
-          </>
-        )}
-      </View>
+          </View>
+
+          <View className="h-[80%]">
+            {isLoading ? (
+              <ActivityIndicator
+                size="large"
+                color="rgb(59 130 246)"
+                className="my-8"
+              />
+            ) : (
+              <>
+                {orders && orders.length > 0 && (
+                  <FlatList
+                    data={orders}
+                    keyExtractor={(order) => order.$id}
+                    contentContainerStyle={{
+                      paddingHorizontal: 16,
+                      paddingBottom: user.role === "admin" || user.role === "moderator" ? 16 : 0,
+                    }}
+                    renderItem={({ item }) => (
+                      <OrderCard
+                        order={item}
+                        refetchOrders={refetchData}
+                      />
+                    )}
+                    refreshControl={
+                      <RefreshControl
+                        refreshing={refreshing}
+                        onRefresh={refetchOrders}
+                      />
+                    }
+                  />
+                )}
+              </>
+            )}
+          </View>
+        </>
+      )}
     </View>
   );
 }
