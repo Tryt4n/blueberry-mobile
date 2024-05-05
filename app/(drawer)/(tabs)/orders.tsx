5b470db79eca4a4e047fac09d319b70b3e5f855a
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-05-05 13:19:40 +0200

    Edited orders page. Moved some logic into its own components, added toast message.

--- app/(drawer)/(tabs)/orders.tsx
+++ app/(drawer)/(tabs)/orders.tsx
@@ -1,49 +1,62 @@
 import { View, Text, FlatList, ActivityIndicator, RefreshControl } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { Ionicons } from "@expo/vector-icons";
-import React, { useState } from "react";
-import { getOrders } from "@/api/appwrite/orders";
+import React, { useEffect, useState } from "react";
 import { useGlobalContext } from "@/hooks/useGlobalContext";
+import { useOrdersContext } from "@/hooks/useOrdersContext";
 import { useBottomSheetContext } from "@/hooks/useBottomSheetContext";
 import { useAppwrite } from "@/hooks/useAppwrite";
+import { getOrders } from "@/api/appwrite/orders";
+import CurrentPrice from "@/components/CurrentPrice";
 import OrderCard from "@/components/OrderCard";
-import CustomButton from "@/components/CustomButton";
+import Toast from "react-native-toast-message";
 
 export default function TabOrders() {
   const { user } = useGlobalContext();
   const { handleOpenBottomSheet } = useBottomSheetContext();
-  const currentPrice = 13;
-  const {
-    data: orders,
-    isLoading,
-    refetchData,
-  } = useAppwrite(
+  const { currentPrice, ordersData, setOrdersData } = useOrdersContext();
+  const [refreshing, setRefreshing] = useState(false);
+
+  const appwriteOrdersData = useAppwrite(
     getOrders,
-    (user && user.role !== "admin") || (user && user.role !== "moderator") ? [user.$id] : [],
+    user && user.role !== "admin" && user.role !== "moderator" ? [user.$id] : [],
     {
       title: "Błąd",
       message: "Nie udało się pobrać zamówień. Spróbuj odświeżyć stronę.",
     }
   );
-  const [refreshing, setRefreshing] = useState(false);
+
+  useEffect(() => {
+    setOrdersData(appwriteOrdersData);
+  }, [appwriteOrdersData.isLoading]);
 
   async function refetchOrders() {
     setRefreshing(true);
 
-    await refetchData();
+    ordersData && (await ordersData.refetchData());
+
+    Toast.show({
+      type: "info",
+      text1: "Zaktualizowano zamówienia",
+      text1Style: { textAlign: "center", fontSize: 16 },
+    });
 
     setRefreshing(false);
   }
 
   return (
     <View className="h-full w-[90%] mx-auto">
-      {user && (user.role === "admin" || user.role === "moderator" || user.role === "seller") && (
+      {user && ordersData && (
         <>
           <View className="mt-8 flex flex-row justify-between pb-2">
-            <Text className="font-poppinsBold text-3xl">Zamówienia</Text>
+            <Text className="font-poppinsBold text-3xl">
+              {user.role === "admin" || user.role === "moderator"
+                ? "Zamówienia"
+                : "Twoje zamówienia"}
+            </Text>
 
             <TouchableOpacity
-              disabled={isLoading}
+              disabled={ordersData.isLoading}
               className="-translate-y-[12px]"
               onPress={handleOpenBottomSheet}
               aria-label="Dodaj nowe zamówienie"
@@ -56,21 +69,10 @@ export default function TabOrders() {
             </TouchableOpacity>
           </View>
 
-          <View className="flex-row items-center gap-4 mb-4">
-            <Text className="font-poppinsRegular text-base">
-              Cena: <Text className="font-poppinsMedium text-xl">{currentPrice} zł</Text>
-            </Text>
-            {(user.role === "admin" || user.role === "moderator") && (
-              <CustomButton
-                text="Zmień cenę"
-                containerStyles=""
-                textStyles="text-xs p-3"
-              />
-            )}
-          </View>
+          <CurrentPrice />
 
           <View className="h-[80%]">
-            {isLoading ? (
+            {ordersData.isLoading ? (
               <ActivityIndicator
                 size="large"
                 color="rgb(59 130 246)"
@@ -78,27 +80,35 @@ export default function TabOrders() {
               />
             ) : (
               <>
-                {orders && orders.length > 0 && (
-                  <FlatList
-                    data={orders}
-                    keyExtractor={(order) => order.$id}
-                    contentContainerStyle={{
-                      paddingHorizontal: 16,
-                      paddingBottom: user.role === "admin" || user.role === "moderator" ? 16 : 0,
-                    }}
-                    renderItem={({ item }) => (
-                      <OrderCard
-                        order={item}
-                        refetchOrders={refetchData}
+                {ordersData.data && currentPrice && (
+                  <>
+                    {ordersData.data.length > 0 ? (
+                      <FlatList
+                        data={ordersData.data}
+                        keyExtractor={(order) => order.$id}
+                        contentContainerStyle={{
+                          paddingHorizontal: 16,
+                          paddingBottom:
+                            user.role === "admin" || user.role === "moderator" ? 16 : 0,
+                        }}
+                        renderItem={({ item }) => (
+                          <OrderCard
+                            order={item}
+                            currentPrice={currentPrice}
+                            refetchOrders={ordersData.refetchData}
+                          />
+                        )}
+                        refreshControl={
+                          <RefreshControl
+                            refreshing={refreshing}
+                            onRefresh={refetchOrders}
+                          />
+                        }
                       />
+                    ) : (
+                      <Text className="text-center font-poppinsRegular text-lg">Brak zamówień</Text>
                     )}
-                    refreshControl={
-                      <RefreshControl
-                        refreshing={refreshing}
-                        onRefresh={refetchOrders}
-                      />
-                    }
-                  />
+                  </>
                 )}
               </>
             )}
