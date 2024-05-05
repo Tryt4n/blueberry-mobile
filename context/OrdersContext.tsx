b07bde875131510d70cb567358f47547c6a87db2
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-05-05 13:23:29 +0200

    Created orders context.

---
+++ context/OrdersContext.tsx
@@ -0,0 +1,38 @@
+import { createContext, useState } from "react";
+import { getCurrentPrice } from "@/api/appwrite/currentPrice";
+import { useAppwrite } from "@/hooks/useAppwrite";
+import type { Order } from "@/types/orders";
+import type { CurrentPrice } from "@/types/currentPrice";
+
+type OrdersDataType = {
+  data: Order[] | undefined;
+  isLoading: boolean;
+  refetchData: () => Promise<void>;
+};
+
+type OrderContextType = {
+  currentPrice: CurrentPrice["price"] | undefined;
+  currentPriceId: CurrentPrice["$id"] | undefined;
+  ordersData: OrdersDataType | null;
+  setOrdersData: React.Dispatch<React.SetStateAction<OrdersDataType | null>>;
+};
+
+export const OrdersContext = createContext<OrderContextType | null>(null);
+
+export default function OrderContextProvider({ children }: { children: React.ReactNode }) {
+  const [ordersData, setOrdersData] = useState<OrderContextType["ordersData"]>(null);
+
+  const { data: currentPriceObj } = useAppwrite(getCurrentPrice, [], {
+    title: "Błąd",
+    message: "Nie udało się pobrać aktualnej ceny.",
+  });
+
+  const contextValues: OrderContextType = {
+    currentPrice: currentPriceObj?.price,
+    currentPriceId: currentPriceObj?.$id,
+    ordersData,
+    setOrdersData,
+  };
+
+  return <OrdersContext.Provider value={contextValues}>{children}</OrdersContext.Provider>;
+}
