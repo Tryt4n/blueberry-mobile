a5ebe7e0271ecded4a28989dae9e3fd7098005d7
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-05-07 12:23:37 +0200

    Created popup menu component.

---
+++ components/OrderCardMenuOptions.tsx
@@ -0,0 +1,52 @@
+import { Menu, MenuOption, MenuOptions, MenuTrigger } from "react-native-popup-menu";
+import { BorderlessButton } from "react-native-gesture-handler";
+import { Ionicons } from "@expo/vector-icons";
+
+type OrderCardMenuOptionsProps = {
+  options: { text: string; onSelect: () => void }[];
+};
+
+export default function OrderCardMenuOptions({ options }: OrderCardMenuOptionsProps) {
+  return (
+    <Menu>
+      <MenuTrigger
+        customStyles={{
+          TriggerTouchableComponent: BorderlessButton,
+        }}
+      >
+        <Ionicons
+          name="ellipsis-vertical"
+          size={32}
+          color="rgb(59 130 246)"
+        />
+      </MenuTrigger>
+
+      <MenuOptions
+        customStyles={{
+          optionsContainer: {
+            width: "auto",
+            paddingHorizontal: 16,
+            paddingVertical: 8,
+            borderRadius: 16,
+          },
+          optionText: {
+            fontFamily: "Poppins-SemiBold",
+            fontSize: 16,
+            color: "black",
+            paddingHorizontal: 16,
+            paddingVertical: 4,
+            textAlign: "center",
+          },
+        }}
+      >
+        {options.map((option, index) => (
+          <MenuOption
+            key={index}
+            text={option.text}
+            onSelect={option.onSelect}
+          />
+        ))}
+      </MenuOptions>
+    </Menu>
+  );
+}
