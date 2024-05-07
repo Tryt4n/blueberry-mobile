6599cfebdf32782322019d800fb83326ab31a3ab
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-05-07 17:08:33 +0200

    Adjusted functionality for editing/deleting orders.

--- components/OrderBottomSheet.tsx
+++ components/OrderBottomSheet.tsx
@@ -1,12 +1,12 @@
-import { View, Alert, type TextInput } from "react-native";
-import React, { useEffect, useRef, useState } from "react";
+import { View, Alert, type TextInput, Dimensions } from "react-native";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { useGlobalContext } from "@/hooks/useGlobalContext";
 import { useBottomSheetContext } from "@/hooks/useBottomSheetContext";
 import { useOrdersContext } from "@/hooks/useOrdersContext";
 import { useAppwrite } from "@/hooks/useAppwrite";
 import { createNewBuyer, getAllBuyers } from "@/api/appwrite/buyers";
-import { createOrder } from "@/api/appwrite/orders";
-import BottomSheet from "@gorhom/bottom-sheet";
+import { createOrder, editOrder } from "@/api/appwrite/orders";
+import BottomSheet, { BottomSheetScrollView } from "@gorhom/bottom-sheet";
 import Toast from "react-native-toast-message";
 import { FormField } from "./FormField";
 import { QuantityInput } from "./QuantityInput";
@@ -20,18 +20,19 @@ export default function OrderBottomSheet() {
   const { bottomSheetModalRef, snapPoints, handleCloseBottomSheet, renderBackdrop } =
     useBottomSheetContext();
   const [quantity, setQuantity] = useState(1);
-  const newOrderDataInitialState = {
+  const orderDataInitialState = {
     quantity: quantity,
     buyerName: "",
     additionalInfo: "",
     userId: user && user.$id,
   };
-  const { currentPriceId, ordersData } = useOrdersContext();
-  const [newOrderData, setNewOrderData] = useState(newOrderDataInitialState);
+  const { currentPriceId, ordersData, editedOrder, setEditedOrder } = useOrdersContext();
+  const [orderData, setOrderData] = useState(orderDataInitialState);
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   const quantityRef = useRef<TextInput>(null);
   const buyerNameRef = useRef<TextInput>(null);
+  const additionalInfoRef = useRef<TextInput>(null);
 
   const {
     data: buyers,
@@ -42,22 +43,22 @@ export default function OrderBottomSheet() {
     message: "Nie udało się pobrać klientów. Spróbuj odświeżyć stronę.",
   });
 
-  async function handleCreateOrder() {
-    if (!newOrderData.quantity) return Alert.alert("Błąd zamówienia", "Wprowadź ilość.");
-    if (newOrderData.buyerName === "")
+  async function handleOrder() {
+    if (!orderData.quantity) return Alert.alert("Błąd zamówienia", "Wprowadź ilość.");
+    if (orderData.buyerName === "")
       return Alert.alert("Błąd zamówienia", "Wprowadź nazwę kupującego.");
 
-    if (user && buyers && newOrderData.quantity && typeof newOrderData.quantity === "number") {
+    if (user && buyers && orderData.quantity && typeof orderData.quantity === "number") {
       setIsSubmitting(true);
 
-      const trimmedBuyerName = newOrderData.buyerName.replace(/\s+/g, " ").trim().toLowerCase(); // trim and lowercase buyer name
+      const trimmedBuyerName = orderData.buyerName.replace(/\s+/g, " ").trim().toLowerCase(); // trim and lowercase buyer name
       const existingBuyer = buyers.find((buyer) => buyer.buyerName === trimmedBuyerName);
 
       try {
         let buyerId: Buyer["$id"];
         if (!existingBuyer) {
           const { buyer: newBuyer, error } = await createNewBuyer(
-            newOrderData.buyerName.toLowerCase()
+            orderData.buyerName.toLowerCase()
           );
 
           if (newBuyer) {
@@ -75,30 +76,41 @@ export default function OrderBottomSheet() {
             "Nie udało się pobrać aktualnej ceny. Spróbuj ponownie."
           );
 
-        const { errors } = await createOrder(
-          user.$id,
-          buyerId,
-          newOrderData.quantity,
-          currentPriceId,
-          newOrderData.additionalInfo
-        );
+        const { errors } = editedOrder
+          ? await editOrder(editedOrder.$id, {
+              userId: editedOrder.user.$id,
+              buyerId: editedOrder.buyer.$id,
+              quantity: orderData.quantity,
+              completed: editedOrder.completed,
+              additionalInfo: orderData.additionalInfo,
+            })
+          : await createOrder(
+              user.$id,
+              buyerId,
+              orderData.quantity,
+              currentPriceId,
+              orderData.additionalInfo
+            );
 
         if (errors) {
           return Alert.alert("Błąd zamówienia", errors.quantity.join("\n"));
         } else {
           handleCloseBottomSheet();
-          setNewOrderData(newOrderDataInitialState);
+          setOrderData(orderDataInitialState);
           setQuantity(1);
           ordersData && ordersData.refetchData();
           refetchData();
           Toast.show({
             type: "success",
-            text1: "Zamówienie zostało utworzone.",
-            text1Style: { textAlign: "center", fontSize: 16 },
+            text1: `Zamówienie zostało ${editedOrder ? "edytowane" : "utworzone"}.`,
+            text1Style: { textAlign: "left", fontSize: 16 },
           });
         }
       } catch (error) {
-        Alert.alert("Błąd", "Nie udało się utworzyć zamówienia. Spróbuj ponownie.");
+        Alert.alert(
+          "Błąd",
+          `Nie udało się ${editedOrder ? "edytować" : "utworzyć"} zamówienia. Spróbuj ponownie.`
+        );
       } finally {
         setIsSubmitting(false);
       }
@@ -106,8 +118,27 @@ export default function OrderBottomSheet() {
   }
 
   useEffect(() => {
-    setNewOrderData({ ...newOrderData, quantity: quantity });
+    setOrderData({ ...orderData, quantity: quantity });
   }, [quantity]);
+
+  useEffect(() => {
+    if (editedOrder) {
+      setOrderData({
+        quantity: editedOrder.quantity,
+        buyerName: editedOrder.buyer.buyerName,
+        additionalInfo: editedOrder.additionalInfo || "",
+        userId: user && user.$id,
+      });
+      setQuantity(editedOrder.quantity);
+    } else {
+      setOrderData(orderDataInitialState);
+      setQuantity(1);
+    }
+  }, [editedOrder]);
+
+  const dropdownHeight = useMemo(() => {
+    return Dimensions.get("window").height * 0.425;
+  }, []);
 
   return (
     <BottomSheet
@@ -122,47 +153,57 @@ export default function OrderBottomSheet() {
       onClose={() => {
         quantityRef.current?.blur();
         buyerNameRef.current?.blur();
+        additionalInfoRef.current?.blur();
         handleCloseBottomSheet();
+        setEditedOrder(null);
+        setQuantity(1);
       }}
-      style={{ paddingHorizontal: 16 }}
+      style={{
+        paddingHorizontal: 16,
+      }}
     >
-      <View className="flex-1">
-        <QuantityInput
-          ref={quantityRef}
-          label="Ilość:"
-          value={newOrderData.quantity}
-          setValue={setQuantity}
-          containerStyles="mt-4"
-        />
+      <BottomSheetScrollView>
+        <View style={{ minHeight: dropdownHeight + 125, zIndex: 1 }}>
+          <QuantityInput
+            ref={quantityRef}
+            label="Ilość:"
+            value={orderData.quantity}
+            setValue={setQuantity}
+            containerStyles="mt-4"
+          />
 
-        <BuyersDropDownPicker
-          ref={buyerNameRef}
-          label="Kupujący:"
-          buyers={buyers}
-          loading={isLoadingBuyers}
-          onChangeValue={(value: ValueType | null) => {
-            if (!value) return;
-            setNewOrderData({ ...newOrderData, buyerName: value as string });
-          }}
-        />
+          <BuyersDropDownPicker
+            ref={buyerNameRef}
+            defaultValue={editedOrder ? editedOrder.buyer.buyerName : undefined}
+            dropDownHeight={dropdownHeight}
+            label="Kupujący:"
+            buyers={buyers}
+            loading={isLoadingBuyers}
+            onChangeValue={(value: ValueType | null) => {
+              if (!value) return;
+              setOrderData({ ...orderData, buyerName: value as string });
+            }}
+          />
 
-        <FormField
-          title="Dodatkowe informacje:"
-          placeholder="Jeśli chcesz to możesz dodać dodatkowe informacje do zamówienia"
-          maxLength={250}
-          value={newOrderData.additionalInfo}
-          multiline={true}
-          numberOfLines={4}
-          handleChangeText={(e) => setNewOrderData({ ...newOrderData, additionalInfo: e })}
-        />
-      </View>
+          <FormField
+            ref={additionalInfoRef}
+            title="Dodatkowe informacje:"
+            placeholder="Jeśli chcesz to możesz dodać dodatkowe informacje do zamówienia"
+            maxLength={250}
+            value={orderData.additionalInfo}
+            multiline={true}
+            numberOfLines={4}
+            handleChangeText={(e) => setOrderData({ ...orderData, additionalInfo: e })}
+          />
+        </View>
 
-      <CustomButton
-        text="Utwórz zamówienie"
-        onPress={handleCreateOrder}
-        loading={isSubmitting}
-        containerStyles="mb-8"
-      />
+        <CustomButton
+          text={editedOrder ? "Zapisz zmiany" : "Utwórz zamówienie"}
+          onPress={handleOrder}
+          loading={isSubmitting}
+          containerStyles="mb-8"
+        />
+      </BottomSheetScrollView>
     </BottomSheet>
   );
 }
