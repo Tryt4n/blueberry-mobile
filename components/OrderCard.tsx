d30db104acb287aad8f531282e194a601a1d852a
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-05-07 17:07:18 +0200

    Added delete order functionality.

--- components/OrderCard.tsx
+++ components/OrderCard.tsx
@@ -1,11 +1,16 @@
 import { View, Text, Alert, Image } from "react-native";
 import { useState } from "react";
 import { useGlobalContext } from "@/hooks/useGlobalContext";
+import { useOrdersContext } from "@/hooks/useOrdersContext";
 import { useBottomSheetContext } from "@/hooks/useBottomSheetContext";
-import { editOrder as editOrderAppwrite } from "@/api/appwrite/orders";
+import {
+  deleteOrder as deleteOrderAppwrite,
+  editOrder as editOrderAppwrite,
+} from "@/api/appwrite/orders";
 import { Entypo } from "@expo/vector-icons";
 import { formatDistanceToNow } from "date-fns";
 import { pl } from "date-fns/locale/pl";
+import Toast from "react-native-toast-message";
 import BouncyCheckbox from "react-native-bouncy-checkbox/build/dist/BouncyCheckbox";
 import OrderCardMenuOptions from "./OrderCardMenuOptions";
 import type { Order } from "@/types/orders";
@@ -18,6 +23,7 @@ type OrderCardProps = {
 
 export default function OrderCard({ order: orderTest, currentPrice }: OrderCardProps) {
   const { user } = useGlobalContext();
+  const { setOrdersData, setEditedOrder } = useOrdersContext();
   const { handleOpenBottomSheet } = useBottomSheetContext();
 
   const [order, setOrder] = useState(orderTest);
@@ -35,29 +41,113 @@ export default function OrderCard({ order: orderTest, currentPrice }: OrderCardP
       setOrder((prevOrder) => ({ ...prevOrder, completed: !prevOrder.completed }));
 
       await editOrderAppwrite(order.$id, updatedOrder);
+
+      Toast.show({
+        type: "success",
+        text1: "Zmieniono status zamówienia na",
+        text2: `${order.completed === false ? "ukończone" : "nieukończone"}.`,
+        text1Style: { textAlign: "left", fontSize: 16 },
+        text2Style: { textAlign: "left", fontSize: 16, fontWeight: "bold", color: "black" },
+      });
     } catch (error) {
       Alert.alert("Błąd", "Nie udało się zaktualizować zamówienia.");
     }
   }
 
-  function editOrder(orderId: Order["$id"], orderCreatorId: Order["user"]["$id"]) {
+  function openEditOrderBottomSheet(orderCreatorId: Order["user"]["$id"]) {
     if (
       user &&
       (user.$id === orderCreatorId || user.role === "admin" || user.role === "moderator")
     ) {
-      console.log("edit");
+      setEditedOrder(order);
       handleOpenBottomSheet();
     }
   }
 
-  async function deleteOrder(orderId: Order["$id"], orderCreatorId: Order["user"]["$id"]) {
+  function deleteOrder(orderId: Order["$id"], orderCreatorId: Order["user"]["$id"]) {
     if (
       user &&
       (user.$id === orderCreatorId || user.role === "admin" || user.role === "moderator")
     ) {
-      console.log("delete");
+      // TODO: Change to custom dialog
+      Alert.alert(
+        "Usuwanie zamówienia",
+        "Czy na pewno chcesz usunąć zamówienie?",
+        [
+          {
+            text: "Usuń",
+            onPress: async () => {
+              try {
+                await deleteOrderAppwrite(orderId);
+
+                setOrdersData((prevData) => {
+                  if (!prevData) return null;
+                  return {
+                    ...prevData,
+                    data: prevData?.data?.filter((order) => order.$id !== orderId),
+                  };
+                });
+
+                Toast.show({
+                  type: "success",
+                  text1: "Zmieniono zostało pomyślnie",
+                  text2: "usunięte.",
+                  text1Style: { textAlign: "left", fontSize: 16 },
+                  text2Style: {
+                    textAlign: "left",
+                    fontSize: 16,
+                    fontWeight: "bold",
+                    color: "black",
+                  },
+                });
+              } catch (error) {
+                Alert.alert("Błąd", "Nie udało się usunąć zamówienia.");
+              }
+            },
+            style: "destructive",
+          },
+          {
+            text: "Anuluj",
+            style: "cancel",
+          },
+        ],
+        { cancelable: true }
+      );
     }
   }
+  // async function deleteOrder(orderId: Order["$id"], orderCreatorId: Order["user"]["$id"]) {
+  //   if (
+  //     user &&
+  //     (user.$id === orderCreatorId || user.role === "admin" || user.role === "moderator")
+  //   ) {
+  //     try {
+  //       await deleteOrderAppwrite(orderId);
+
+  //       setOrdersData((prevData) => {
+  //         if (!prevData) return null;
+  //         return {
+  //           ...prevData,
+  //           data: prevData?.data?.filter((order) => order.$id !== orderId),
+  //         };
+  //       });
+
+  //       Toast.show({
+  //         type: "success",
+  //         text1: "Zmieniono zostało pomyślnie",
+  //         text2: "usunięte.",
+  //         text1Style: { textAlign: "left", fontSize: 16 },
+  //         text2Style: { textAlign: "left", fontSize: 16, fontWeight: "bold", color: "black" },
+  //       });
+  //     } catch (error) {
+  //       Alert.alert("Błąd", "Nie udało się usunąć zamówienia.");
+  //     }
+  //   }
+  // }
+
+  const orderOptions = [
+    { text: "Edytuj", onSelect: () => openEditOrderBottomSheet(order.user.$id) },
+    { text: "Usuń", onSelect: () => deleteOrder(order.$id, order.user.$id) },
+  ];
 
   return (
     <>
@@ -113,12 +203,7 @@ export default function OrderCard({ order: orderTest, currentPrice }: OrderCardP
           )}
 
           <View className="flex flex-row-reverse justify-between items-end">
-            <OrderCardMenuOptions
-              options={[
-                { text: "Edytuj", onSelect: () => editOrder(order.$id, order.user.$id) },
-                { text: "Usuń", onSelect: () => deleteOrder(order.$id, order.user.$id) },
-              ]}
-            />
+            <OrderCardMenuOptions options={orderOptions} />
 
             <View className="mt-8">
               {(user.role === "admin" || user.role === "moderator") && (
