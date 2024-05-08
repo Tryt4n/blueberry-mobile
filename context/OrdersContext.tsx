778419a6ffe15c96df647692c86d4d77330baeba
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-05-08 09:47:56 +0200

    Moved `deleteOrder` from `OrderCard.tsx`.

--- context/OrdersContext.tsx
+++ context/OrdersContext.tsx
@@ -1,6 +1,9 @@
-import { createContext, useState } from "react";
+import { createContext, useCallback, useState } from "react";
+import { Alert } from "react-native";
 import { getCurrentPrice } from "@/api/appwrite/currentPrice";
+import { deleteOrder as deleteOrderAppwrite } from "@/api/appwrite/orders";
 import { useAppwrite } from "@/hooks/useAppwrite";
+import Toast from "react-native-toast-message";
 import type { Order } from "@/types/orders";
 import type { CurrentPrice } from "@/types/currentPrice";
 
@@ -17,6 +20,7 @@ type OrderContextType = {
   setOrdersData: React.Dispatch<React.SetStateAction<OrdersDataType | null>>;
   editedOrder: Order | null;
   setEditedOrder: (order: Order | null) => void;
+  deleteOrder: (orderId: Order["$id"]) => Promise<void>;
 };
 
 export const OrdersContext = createContext<OrderContextType | null>(null);
@@ -30,6 +34,38 @@ export default function OrderContextProvider({ children }: { children: React.Rea
     message: "Nie udało się pobrać aktualnej ceny.",
   });
 
+  const deleteOrder = useCallback(
+    async (orderId: Order["$id"]) => {
+      try {
+        await deleteOrderAppwrite(orderId);
+
+        setOrdersData((prevData) => {
+          if (!prevData) return null;
+          return {
+            ...prevData,
+            data: prevData?.data?.filter((order) => order.$id !== orderId),
+          };
+        });
+
+        Toast.show({
+          type: "success",
+          text1: "Zamówienie zostało pomyślnie",
+          text2: "usunięte.",
+          text1Style: { textAlign: "left", fontSize: 16 },
+          text2Style: {
+            textAlign: "left",
+            fontSize: 16,
+            fontWeight: "bold",
+            color: "black",
+          },
+        });
+      } catch (error) {
+        Alert.alert("Błąd", "Nie udało się usunąć zamówienia.");
+      }
+    },
+    [deleteOrderAppwrite]
+  );
+
   const contextValues: OrderContextType = {
     currentPrice: currentPriceObj?.price,
     currentPriceId: currentPriceObj?.$id,
@@ -37,6 +73,7 @@ export default function OrderContextProvider({ children }: { children: React.Rea
     setOrdersData,
     editedOrder,
     setEditedOrder,
+    deleteOrder,
   };
 
   return <OrdersContext.Provider value={contextValues}>{children}</OrdersContext.Provider>;
