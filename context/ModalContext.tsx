19cbb106d6f278fe67fcbf8080ba7cd4c872d676
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-05-13 16:19:08 +0200

    Changed modal context logic to use `react-native-paper` modal. Deleted input state and moved.

--- context/ModalContext.tsx
+++ context/ModalContext.tsx
@@ -1,72 +1,37 @@
-import { createContext, useEffect, useRef, useState } from "react";
-import type { ModalDataType } from "@/components/Modal";
+import { createContext, useState } from "react";
+import type { ModalProps } from "@/components/Modal";
 
 type ModalContextType = {
   visible: boolean;
   showModal: () => void;
-  handleCancel: () => void;
-  handleConfirmation: () => void;
-  modalData: ModalDataType;
-  setModalData: (data: ModalDataType | ((prevData: ModalDataType) => ModalDataType)) => void;
-  inputValue: string;
-  setInputValue: (value: string) => void;
+  closeModal: () => void;
+  modalData: ModalProps;
+  setModalData: (data: ModalProps | ((prevData: ModalProps) => ModalProps)) => void;
 };
 
 export const ModalContext = createContext<ModalContextType | null>(null);
 
 export default function ModalContextProvider({ children }: { children: React.ReactNode }) {
   const [visible, setVisible] = useState(false);
-  const modalDataInitialState: ModalDataType = {
+  const modalDataInitialState: ModalProps = {
     title: "",
-    description: "",
-    btn1: {
-      text: "Ok",
-      type: "confirm",
-      color: "primary",
-    },
   };
-  const [modalData, setModalData] = useState(modalDataInitialState);
-  const [inputValue, setInputValue] = useState("");
-
-  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+  const [modalData, setModalData] = useState<ModalProps>(modalDataInitialState);
 
   function showModal() {
     setVisible(true);
   }
 
-  function handleCancel() {
-    modalData.btn2 && modalData.btn2.onPress && modalData.btn2.onPress();
+  function closeModal() {
     setVisible(false);
-    timeoutRef.current = setTimeout(() => {
-      setModalData(modalDataInitialState);
-    }, 500);
   }
-
-  function handleConfirmation() {
-    modalData.btn1.onPress && modalData.btn1.onPress();
-    setVisible(false);
-    timeoutRef.current = setTimeout(() => {
-      setModalData(modalDataInitialState);
-    }, 500);
-  }
-
-  useEffect(() => {
-    return () => {
-      if (timeoutRef.current) {
-        clearTimeout(timeoutRef.current);
-      }
-    };
-  }, []);
 
   const contextValues: ModalContextType = {
     visible,
     showModal,
-    handleCancel,
-    handleConfirmation,
+    closeModal,
     modalData,
     setModalData,
-    inputValue,
-    setInputValue,
   };
 
   return <ModalContext.Provider value={contextValues}>{children}</ModalContext.Provider>;
