11d3f1069c5d7eaa2840751b11bc4f94a678b34a
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-05-14 09:17:53 +0200

    Moved file to another directory. Extracted some logic into its own components.

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,7 +1,10 @@
-import { View, Text, TouchableOpacity, TextInput } from "react-native";
+import { View, Text, TextInput } from "react-native";
 import { Modal as ReactNativePaperModal, Portal } from "react-native-paper";
 import { useModalContext } from "@/hooks/useModalContext";
+import ModalButton from "./ModalButton";
+import ModalInput from "./ModalInput";
 import type { ComponentProps } from "react";
+import type { ModalButtonType } from "./ModalButton";
 
 export type ModalProps = {
   title: string;
@@ -10,14 +13,6 @@ export type ModalProps = {
   btn2?: ModalButtonType;
   input?: ComponentProps<typeof TextInput>;
 };
-
-type ModalButtonType = {
-  text?: string;
-  color?: ModalButtonColorType;
-  onPress?: () => void;
-};
-
-type ModalButtonColorType = "danger" | "primary" | "default";
 
 export default function Modal() {
   const { visible, closeModal, modalData } = useModalContext();
@@ -40,26 +35,18 @@ export default function Modal() {
 
         {subtitle && <Text className="font-poppinsMedium text-base">{subtitle}</Text>}
 
-        {input && (
-          <View className="flex justify-center items-center mt-2">
-            <TextInput
-              className="w-32 border-b-2 text-center font-poppinsMedium text-3xl"
-              cursorColor="rgb(59 130 246)"
-              {...input}
-            />
-          </View>
-        )}
+        {input && <ModalInput {...input} />}
 
         <View className="flex flex-row justify-end mt-4">
           {btn1 && (
-            <DialogButton
+            <ModalButton
               btn={btn1}
               closeModalFn={closeModal}
             />
           )}
 
           {btn2 && (
-            <DialogButton
+            <ModalButton
               btn={btn2}
               closeModalFn={closeModal}
             />
@@ -67,30 +54,5 @@ export default function Modal() {
         </View>
       </ReactNativePaperModal>
     </Portal>
-  );
-}
-
-function DialogButton({ btn, closeModalFn }: { btn: ModalButtonType; closeModalFn: () => void }) {
-  return (
-    <TouchableOpacity
-      onPress={() => {
-        btn.onPress && btn.onPress();
-        closeModalFn();
-      }}
-    >
-      <Text
-        className="font-poppinsMedium text-base px-2"
-        style={{
-          color:
-            btn.color && btn.color === "danger"
-              ? "#FF3333"
-              : btn.color === "primary"
-              ? "rgb(59 130 246)"
-              : "black",
-        }}
-      >
-        {btn.text}
-      </Text>
-    </TouchableOpacity>
   );
 }
