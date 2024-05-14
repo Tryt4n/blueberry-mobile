53bdd37241b019348d8dc69d5e74a73853f3530f
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-05-14 09:20:02 +0200

    Created orders list component.

---
+++ components/Orders/OrdersList.tsx
@@ -0,0 +1,55 @@
+import { FlashList } from "@shopify/flash-list";
+import { RefreshControl } from "react-native";
+import { useState } from "react";
+import { useOrdersContext } from "@/hooks/useOrdersContext";
+import { useGlobalContext } from "@/hooks/useGlobalContext";
+import OrderCard from "./OrderCard";
+import Toast from "react-native-toast-message";
+
+export default function OrdersList() {
+  const { user } = useGlobalContext();
+  const { currentPrice, ordersData } = useOrdersContext();
+  const [refreshing, setRefreshing] = useState(false);
+
+  async function refetchOrders() {
+    setRefreshing(true);
+
+    ordersData && (await ordersData.refetchData());
+
+    Toast.show({
+      type: "info",
+      text1: "Zaktualizowano zamówienia",
+      text1Style: { textAlign: "center", fontSize: 16 },
+    });
+
+    setRefreshing(false);
+  }
+
+  return (
+    <>
+      {user && ordersData && currentPrice && (
+        <FlashList
+          data={ordersData.data}
+          keyExtractor={(order) => order.$id}
+          contentContainerStyle={{
+            paddingHorizontal: 16,
+            paddingBottom: user.role === "admin" || user.role === "moderator" ? 16 : 0,
+          }}
+          renderItem={({ item }) => (
+            <OrderCard
+              order={item}
+              price={currentPrice.price}
+            />
+          )}
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={refetchOrders}
+            />
+          }
+          estimatedItemSize={10}
+        />
+      )}
+    </>
+  );
+}
