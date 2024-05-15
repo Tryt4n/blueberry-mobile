5b707791600fddaa33ed9ff77d0980edb6b13f28
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-05-15 13:18:34 +0200

    Added additional `onDismiss` prop and added `<Calendar/>` component.

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,10 +1,12 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, type TextInput } from "react-native";
 import { Modal as ReactNativePaperModal, Portal } from "react-native-paper";
 import { useModalContext } from "@/hooks/useModalContext";
 import ModalButton from "./ModalButton";
 import ModalInput from "./ModalInput";
+import ModalCalendar from "./ModalCalendar";
 import type { ComponentProps } from "react";
 import type { ModalButtonType } from "./ModalButton";
+import type { Calendar } from "react-native-calendars";
 
 export type ModalProps = {
   title: string;
@@ -12,17 +14,26 @@ export type ModalProps = {
   btn1?: ModalButtonType;
   btn2?: ModalButtonType;
   input?: ComponentProps<typeof TextInput>;
+  calendar?: ComponentProps<typeof Calendar>;
+  onDismiss?: () => void;
 };
 
 export default function Modal() {
   const { visible, closeModal, modalData } = useModalContext();
-  const { title, subtitle, btn1, btn2, input } = modalData;
+  const { title, subtitle, btn1, btn2, input, calendar, onDismiss } = modalData;
 
   return (
     <Portal>
       <ReactNativePaperModal
         visible={visible}
-        onDismiss={closeModal}
+        onDismiss={
+          onDismiss
+            ? () => {
+                closeModal();
+                onDismiss();
+              }
+            : closeModal
+        }
         contentContainerStyle={{
           backgroundColor: "white",
           padding: 24,
@@ -36,6 +47,8 @@ export default function Modal() {
         {subtitle && <Text className="font-poppinsMedium text-base">{subtitle}</Text>}
 
         {input && <ModalInput {...input} />}
+
+        {calendar && <ModalCalendar {...calendar} />}
 
         <View className="flex flex-row justify-end mt-4">
           {btn1 && (
