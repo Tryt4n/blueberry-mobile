e05e3643f2246dc4004c676491f5db9d8ceb1a2f
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-05-15 13:18:48 +0200

    Created calendar component for modal.

---
+++ components/Modal/ModalCalendar.tsx
@@ -0,0 +1,61 @@
+import { Calendar } from "react-native-calendars";
+import { LocaleConfig } from "react-native-calendars";
+import { FontAwesome } from "@expo/vector-icons";
+import type { ComponentProps } from "react";
+
+export default function ModalCalendar({ ...props }: ComponentProps<typeof Calendar>) {
+  LocaleConfig.locales["pl"] = {
+    monthNames: [
+      "Styczeń",
+      "Luty",
+      "Marzec",
+      "Kwiecień",
+      "Maj",
+      "Czerwiec",
+      "Lipiec",
+      "Sierpień",
+      "Wrzesień",
+      "Październik",
+      "Listopad",
+      "Grudzień",
+    ],
+    monthNamesShort: [
+      "Sty",
+      "Lut",
+      "Mar",
+      "Kwi",
+      "Maj",
+      "Cze",
+      "Lip",
+      "Sie",
+      "Wrz",
+      "Paź",
+      "Lis",
+      "Gru",
+    ],
+    dayNames: ["Niedziela", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota"],
+    dayNamesShort: ["Nd", "Pn", "Wt", "Śr", "Czw", "Pt", "Sob"],
+    today: "Dzisiaj",
+  };
+  LocaleConfig.defaultLocale = "pl";
+
+  return (
+    <Calendar
+      {...props}
+      firstDay={1}
+      monthFormat="MMMM yyyy"
+      enableSwipeMonths={true}
+      renderArrow={(direction) => <Arrow direction={direction} />}
+    />
+  );
+}
+
+function Arrow({ direction }: { direction: "left" | "right" }) {
+  return (
+    <FontAwesome
+      name={direction === "left" ? "caret-left" : "caret-right"}
+      size={16}
+      color="rgb(59 130 246)"
+    />
+  );
+}
