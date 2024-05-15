8470cb7c1289d58eb72c7326140cde9c3645e02a
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-05-15 13:19:11 +0200

    Added additional styling for focused input.

--- components/Modal/ModalInput.tsx
+++ components/Modal/ModalInput.tsx
@@ -1,12 +1,17 @@
 import { View, TextInput } from "react-native";
-import type { ComponentProps } from "react";
+import { useState, type ComponentProps } from "react";
 
 export default function ModalInput({ ...props }: ComponentProps<typeof TextInput>) {
+  const [isFocused, setIsFocused] = useState(false);
+
   return (
     <View className="flex justify-center items-center mt-2">
       <TextInput
         className="w-32 border-b-2 text-center font-poppinsMedium text-3xl"
+        style={{ borderColor: isFocused ? "rgb(59 130 246)" : undefined }}
         cursorColor="rgb(59 130 246)"
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
         {...props}
       />
     </View>
