06d03da4dfd83e1b6fc99260c62ecd389466ee7a
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-05-22 09:25:03 +0200

    Deleted unnecessary imports.

--- context/BottomSheetTabsContext.tsx
+++ context/BottomSheetTabsContext.tsx
@@ -1,12 +1,10 @@
-import { BottomSheetBackdrop, type BottomSheetBackdropProps } from "@gorhom/bottom-sheet";
-import { createContext, useCallback, useRef } from "react";
+import { createContext, useRef } from "react";
 import type { ActionSheetRef } from "react-native-actions-sheet";
 
 type BottomSheetTabsContextType = {
   bottomSheetModalRef: React.RefObject<ActionSheetRef>;
   handleOpenBottomSheet: () => void;
   handleCloseBottomSheet: () => void;
-  renderBackdrop: (props: BottomSheetBackdropProps) => React.ReactNode;
 };
 
 export const BottomSheetTabsContext = createContext<BottomSheetTabsContextType | null>(null);
@@ -21,23 +19,10 @@ export default function BottomSheetTabsContextProvider({
   const handleOpenBottomSheet = () => bottomSheetModalRef.current?.show();
   const handleCloseBottomSheet = () => bottomSheetModalRef.current?.hide();
 
-  const renderBackdrop = useCallback(
-    (props: BottomSheetBackdropProps) => (
-      <BottomSheetBackdrop
-        appearsOnIndex={0}
-        disappearsOnIndex={-1}
-        {...props}
-        onPress={handleCloseBottomSheet}
-      />
-    ),
-    []
-  );
-
   const contextValues: BottomSheetTabsContextType = {
     bottomSheetModalRef,
     handleOpenBottomSheet,
     handleCloseBottomSheet,
-    renderBackdrop,
   };
 
   return (
