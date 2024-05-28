d51348ac9e13a7d16bb02b2cdb9ab858603172b6
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-05-28 14:15:52 +0200

    Added functions to updated user data - email, username, password, avatar.

--- api/appwrite/users.ts
+++ api/appwrite/users.ts
@@ -1,4 +1,6 @@
-import { appwriteConfig, databases } from "@/lib/appwrite";
+import { account, appwriteConfig, databases } from "@/lib/appwrite";
+import { EmailSchema, PasswordSchema, UsernameSchema } from "@/lib/zod/auth";
+import { Query } from "react-native-appwrite";
 import type { User } from "@/types/user";
 
 export async function getListOfUsers() {
@@ -9,6 +11,119 @@ export async function getListOfUsers() {
     );
 
     return users.documents as User[];
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
+
+export async function editUserEmail(userId: User["$id"], email: string, password: string) {
+  let errors: string[] = [];
+
+  try {
+    const results = EmailSchema.safeParse(email);
+    if (!results.success) {
+      results.error.issues.forEach((issue) => {
+        errors = [...errors, issue.message];
+      });
+
+      return errors;
+    } else {
+      const isEmailUnique = await databases.listDocuments(
+        appwriteConfig.databaseId,
+        appwriteConfig.userCollectionId,
+        [Query.equal("email", email.toLowerCase())]
+      );
+      if (isEmailUnique.documents.length > 0) {
+        errors = ["Użytkownik z takim samym adresem email już istnieje."];
+
+        return errors;
+      } else {
+        // Has to be first because of the password confirmation
+        await account.updateEmail(email, password);
+
+        await databases.updateDocument(
+          appwriteConfig.databaseId,
+          appwriteConfig.userCollectionId,
+          userId,
+          {
+            email,
+          }
+        );
+      }
+    }
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
+
+export async function editUserUsername(userId: User["$id"], username: string) {
+  let errors: string[] = [];
+
+  try {
+    const results = UsernameSchema.safeParse(username);
+    if (!results.success) {
+      results.error.issues.forEach((issue) => {
+        errors = [...errors, issue.message];
+      });
+
+      return errors;
+    } else {
+      const isUsernameUnique = await databases.listDocuments(
+        appwriteConfig.databaseId,
+        appwriteConfig.userCollectionId,
+        [Query.equal("username", username)]
+      );
+      if (isUsernameUnique.documents.length > 0) {
+        errors = ["Użytkownik z taką nazwą już istnieje."];
+
+        return errors;
+      } else {
+        await databases.updateDocument(
+          appwriteConfig.databaseId,
+          appwriteConfig.userCollectionId,
+          userId,
+          {
+            username,
+          }
+        );
+
+        await account.updateName(username);
+      }
+    }
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
+
+export async function editUserPassword(newPassword: string, oldPassword: string) {
+  let errors: string[] = [];
+
+  try {
+    const results = PasswordSchema.safeParse(newPassword);
+    if (!results.success) {
+      results.error.issues.forEach((issue) => {
+        errors = [...errors, issue.message];
+      });
+
+      return errors;
+    } else {
+      await account.updatePassword(newPassword, oldPassword);
+    }
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
+
+export async function editUserAvatar(userId: User["$id"], avatar: string) {
+  try {
+    await databases.updateDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      userId,
+      {
+        avatar,
+      }
+    );
   } catch (error: any) {
     throw new Error(error);
   }
