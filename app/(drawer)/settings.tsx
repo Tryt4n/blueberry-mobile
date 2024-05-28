585e376ab54220e1bb1fe5a2d6fec969a6186c76
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-05-28 14:16:17 +0200

    Created settings page.

--- app/(drawer)/settings.tsx
+++ app/(drawer)/settings.tsx
@@ -1,10 +1,72 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, ScrollView } from "react-native";
+import { useState } from "react";
+import { useGlobalContext } from "@/hooks/useGlobalContext";
+import tw from "@/lib/twrnc";
+import { Divider } from "react-native-paper";
+import SettingsEditSection from "@/components/Settings/SettingsEditSection";
+import type { EditSettingsOptions } from "@/types/editSettingsOptions";
+import SettingsEditAvatar from "@/components/Settings/SettingsEditAvatar";
+import SettingsChangeTheme from "@/components/Settings/SettingsChangeTheme";
 
 export default function Settings() {
+  const { user } = useGlobalContext();
+
+  const [inputVisible, setInputVisible] = useState<EditSettingsOptions | false>(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const settingsEditSectionProps = {
+    inputVisible,
+    setInputVisible,
+    isSubmitting,
+    setIsSubmitting,
+  };
+
   return (
-    <View>
-      <Text>Settings</Text>
-    </View>
+    <ScrollView
+      contentContainerStyle={{
+        marginVertical: 64,
+        width: "100%",
+        maxWidth: 700,
+        marginHorizontal: "auto",
+      }}
+    >
+      {user && (
+        <View style={tw`w-[90%] mx-auto`}>
+          <SettingsEditSection
+            type="email"
+            {...settingsEditSectionProps}
+          />
+
+          <Divider style={tw`my-6`} />
+
+          <SettingsEditSection
+            type="username"
+            {...settingsEditSectionProps}
+          />
+
+          {
+            // @ts-ignore - secretPassword is not in the User type but it's in the API response
+            !user.secretPassword && (
+              <>
+                <Divider style={tw`my-6`} />
+
+                <SettingsEditSection
+                  type="password"
+                  {...settingsEditSectionProps}
+                />
+              </>
+            )
+          }
+
+          <Divider style={tw`my-6`} />
+
+          <SettingsEditAvatar />
+
+          <Divider style={tw`my-6`} />
+
+          <SettingsChangeTheme />
+        </View>
+      )}
+    </ScrollView>
   );
 }
