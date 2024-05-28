31e76bda3669928c0789cb211aafda4e0aa3b9aa
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-05-28 15:16:20 +0200

    Created file with all avatar images import.

---
+++ constants/avatars.js
@@ -0,0 +1,31 @@
+import avatar1 from "../assets/avatars/1.png";
+import avatar2 from "../assets/avatars/2.png";
+import avatar3 from "../assets/avatars/3.png";
+import avatar4 from "../assets/avatars/4.png";
+import avatar5 from "../assets/avatars/5.png";
+import avatar6 from "../assets/avatars/6.png";
+import avatar7 from "../assets/avatars/7.png";
+import avatar8 from "../assets/avatars/8.png";
+import avatar9 from "../assets/avatars/9.png";
+import avatar10 from "../assets/avatars/10.png";
+import avatar11 from "../assets/avatars/11.png";
+import avatar12 from "../assets/avatars/12.png";
+import avatar13 from "../assets/avatars/13.png";
+import avatar14 from "../assets/avatars/14.png";
+
+export const avatarImages = [
+  avatar1,
+  avatar2,
+  avatar3,
+  avatar4,
+  avatar5,
+  avatar6,
+  avatar7,
+  avatar8,
+  avatar9,
+  avatar10,
+  avatar11,
+  avatar12,
+  avatar13,
+  avatar14,
+];
