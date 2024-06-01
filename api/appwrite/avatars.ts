71326b3305b52dcc3bba208eb358fe7df1534ac7
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-06-01 12:21:21 +0200

    Fixed bug with uploading images on mobile app.

--- api/appwrite/avatars.ts
+++ api/appwrite/avatars.ts
@@ -1,11 +1,25 @@
+import { Platform } from "react-native";
 import { appwriteConfig, storage } from "@/lib/appwrite";
 import { ID } from "react-native-appwrite";
 import { uriToBlob } from "@/helpers/conversion";
 import type { ImagePickerAsset } from "expo-image-picker";
+import type { DocumentPickerAsset } from "expo-document-picker";
 
-export async function uploadCustomAvatar(customAvatar: ImagePickerAsset) {
+export async function uploadCustomAvatar(customAvatar: ImagePickerAsset | DocumentPickerAsset) {
   try {
-    const file = await uriToBlob(customAvatar.uri, customAvatar.fileName!);
+    let file: File;
+
+    if (Platform.OS === "web") {
+      if ("fileName" in customAvatar) {
+        file = await uriToBlob(customAvatar.uri, customAvatar.fileName!);
+      } else {
+        throw new Error("Nie udało się przesłać pliku. Brak nazwy pliku.");
+      }
+    } else {
+      const { mimeType, ...rest } = customAvatar;
+      file = { type: mimeType!, ...rest } as unknown as File;
+    }
+
     const avatarImage = await storage.createFile(
       appwriteConfig.avatarsStorageId,
       ID.unique(),
@@ -13,7 +27,6 @@ export async function uploadCustomAvatar(customAvatar: ImagePickerAsset) {
     );
     return avatarImage;
   } catch (error) {
-    console.log(error);
     throw new Error("Nie udało się przesłać pliku. Spróbuj ponownie.");
   }
 }
