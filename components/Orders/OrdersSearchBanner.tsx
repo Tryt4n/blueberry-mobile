be175a0ea750c95290dd02732c87a22a0e6550a0
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-06-04 13:54:37 +0200

    Changed orders refetching logic after selecting new date range.

--- components/Orders/OrdersSearchBanner.tsx
+++ components/Orders/OrdersSearchBanner.tsx
@@ -1,101 +1,37 @@
 import { View, Text, useWindowDimensions } from "react-native";
-import { Banner, type BannerProps } from "react-native-paper";
-import { useCallback, useMemo } from "react";
+import { Banner } from "react-native-paper";
+import { useMemo } from "react";
 import { useGlobalContext } from "@/hooks/useGlobalContext";
 import { useThemeContext } from "@/hooks/useThemeContext";
-import { useModalContext } from "@/hooks/useModalContext";
 import { useOrdersContext } from "@/hooks/useOrdersContext";
-import { colors as customColors } from "@/constants/colors";
 import tw from "@/lib/twrnc";
 import OrdersSearchBannerDates from "./OrdersSearchBannerDates";
 import OrdersSearchBannerUser from "./OrdersSearchBannerUser";
-import Toast from "react-native-toast-message";
 
 export default function OrdersSearchBanner() {
   const { user } = useGlobalContext();
-  const { theme, colors } = useThemeContext();
-  const { setModalData, showModal } = useModalContext();
-  const { isBannerVisible, setIsBannerVisible, ordersSearchParams, ordersData } =
-    useOrdersContext();
+  const { colors } = useThemeContext();
+  const { isBannerVisible, setIsBannerVisible } = useOrdersContext();
   const { width } = useWindowDimensions();
 
   const containerWidth = useMemo(() => {
     return width * 0.9 - 32 <= 700 ? width * 0.9 - 32 : 700 - 32;
   }, [width]);
 
-  // Fetch new orders based on the selected date range
-  const getNewOrders = useCallback(() => {
-    if (!ordersSearchParams.startDate && !ordersSearchParams.endDate) {
-      setModalData({
-        title: "Błąd",
-        subtitle: "Wybierz zakres dat przed wyszukaniem.",
-        btn1: { text: "Ok" },
-      });
-      showModal();
-      return;
-    }
-    if (!ordersSearchParams.startDate) {
-      setModalData({
-        title: "Błąd",
-        subtitle: "Wybierz datę początkową.",
-        btn1: { text: "Ok" },
-      });
-      showModal();
-      return;
-    }
-    if (!ordersSearchParams.endDate) {
-      setModalData({
-        title: "Błąd",
-        subtitle: "Wybierz datę końcową.",
-        btn1: { text: "Ok" },
-      });
-      showModal();
-      return;
-    }
-
-    if (ordersData?.isLoading) return;
-
-    if (ordersData) {
-      // Refetch the data with the new parameters
-      ordersData.refetchData();
-
-      // Hide the banner
-      setIsBannerVisible(false);
-
-      // Show a success message
-      Toast.show({
-        type: theme === "light" ? "info" : "infoDark",
-        text1: "Zamówienia zostały zaktualizowane",
-        text2: "o wybrane parametry.",
-        text2Style: { fontSize: 14 },
-      });
-    }
-  }, [ordersSearchParams, ordersData, setIsBannerVisible]);
-
-  // Actions for the <Banner /> component
-  const bannerActions: BannerProps["actions"] = [
-    {
-      label: "Schowaj",
-      onPress: () => setIsBannerVisible(false),
-      labelStyle: { color: colors.text, fontFamily: "Poppins-SemiBold", fontSize: 16 },
-    },
-    {
-      label: "Wyszukaj",
-      onPress: getNewOrders,
-      labelStyle: {
-        color: customColors.primaryLight,
-        fontFamily: "Poppins-SemiBold",
-        fontSize: 16,
-      },
-    },
-  ];
-
   return (
     <Banner
       visible={isBannerVisible}
-      actions={bannerActions}
+      actions={[
+        {
+          label: "Schowaj",
+          onPress: () => setIsBannerVisible(false),
+          labelStyle: { color: colors.danger, fontFamily: "Poppins-SemiBold", fontSize: 16 },
+        },
+      ]}
       elevation={3}
-      style={tw`my-4 bg-[${colors.bg}] rounded-2xl w-full max-w-[700px] mx-auto`}
+      style={tw`bg-[${colors.bg}] rounded-2xl w-full max-w-[700px] mx-auto${
+        isBannerVisible ? " mt-4 my-6" : ""
+      }`}
     >
       <Text style={[tw`font-poppinsBold text-[${colors.text}]`, { fontSize: 24 }]}>
         Wyszukaj zamówienia
