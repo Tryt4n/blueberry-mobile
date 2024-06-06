f8b7845b60fd0714253136f5964d6b7a05030cad
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-06-06 15:43:50 +0200

    Moved logic into `useSelectUser` hook.

--- components/Orders/OrdersSearchBannerUser.tsx
+++ components/Orders/OrdersSearchBannerUser.tsx
@@ -1,91 +1,14 @@
 import { View, Text, TouchableOpacity } from "react-native";
-import React, { useCallback, useEffect, useState } from "react";
-import { useGlobalContext } from "@/hooks/useGlobalContext";
 import { useThemeContext } from "@/hooks/useThemeContext";
 import { useOrdersContext } from "@/hooks/useOrdersContext";
-import { useModalContext } from "@/hooks/useModalContext";
-import { useAppwrite } from "@/hooks/useAppwrite";
+import { useSelectUser } from "@/hooks/OrderHooks/useSelectUser";
 import tw from "@/lib/twrnc";
-import { getListOfUsers } from "@/api/appwrite/users";
-import { UsersDropDownPicker } from "./UsersDropDownPicker";
 import { Entypo, Ionicons } from "@expo/vector-icons";
-import type { User } from "@/types/user";
 
 export default function OrdersSearchBannerUser() {
-  const { user } = useGlobalContext();
   const { colors } = useThemeContext();
   const { ordersSearchParams, setOrdersSearchParams } = useOrdersContext();
-  const { showModal, setModalData } = useModalContext();
-
-  const [searchedUserId, setSearchedUserId] = useState<User["$id"] | undefined>();
-
-  const userHasAccess = user?.role === "admin" || user?.role === "moderator";
-
-  // Fetch the list of users if the user has access
-  const fetchedListOfUsers =
-    userHasAccess &&
-    useAppwrite(getListOfUsers, [], {
-      title: "Błąd",
-      message: "Nie udało się pobrać klientów. Spróbuj odświeżyć stronę.",
-    });
-
-  const openSelectUserModal = useCallback(() => {
-    if (!fetchedListOfUsers) {
-      setModalData({
-        title: "Błąd",
-        subtitle: "Nie udało się pobrać listy użytkowników.",
-        btn1: {
-          text: "Anuluj",
-        },
-      });
-      showModal();
-      return;
-    }
-
-    setModalData({
-      title: "Wybierz użytkownika którego chcesz zobaczyć zamówienia",
-      btn1: {
-        text: "Anuluj",
-      },
-      btn2: {
-        text: "Wybierz",
-        color: "primary",
-      },
-      children: (
-        <UsersDropDownPicker
-          users={fetchedListOfUsers.data}
-          loading={fetchedListOfUsers.isLoading}
-          defaultValue={ordersSearchParams.userId}
-          onChangeValue={(value: string | null) => {
-            if (!value || !userHasAccess) return;
-            setSearchedUserId(value);
-          }}
-        />
-      ),
-    });
-    showModal();
-  }, [fetchedListOfUsers, setModalData, showModal, user, userHasAccess]);
-
-  // Reset the searchedUserId in the modal
-  const saveSearchedUser = useCallback(() => {
-    setOrdersSearchParams((prevState) => ({
-      ...prevState,
-      userId: searchedUserId,
-    }));
-  }, [searchedUserId, setOrdersSearchParams]);
-
-  // Update the saveSearchedUser function in the modal when searchedUserId changes
-  useEffect(() => {
-    if (!fetchedListOfUsers) return;
-
-    setModalData((prevModalData) => ({
-      ...prevModalData,
-      btn2: {
-        ...prevModalData.btn2,
-        onPress: saveSearchedUser,
-      },
-    }));
-  }, [searchedUserId, ordersSearchParams.userId]);
+  const { fetchedListOfUsers, openSelectUserModal } = useSelectUser();
 
   return (
     <>
