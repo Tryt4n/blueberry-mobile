bfcc0e168de8b8479385ab3186d852760b0b8055
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-06-06 15:41:52 +0200

    Moved logic into `useChangeCheckboxValue`.

--- components/Orders/OrderCardIssuedCheckbox.tsx
+++ components/Orders/OrderCardIssuedCheckbox.tsx
@@ -1,62 +1,16 @@
-import React, { useCallback } from "react";
+import { memo } from "react";
 import { useGlobalContext } from "@/hooks/useGlobalContext";
 import { useThemeContext } from "@/hooks/useThemeContext";
-import { editOrder } from "@/api/appwrite/orders";
+import { useChangeCheckboxValue } from "@/hooks/OrderHooks/useChangeCheckboxValue";
 import Checkbox from "../Checkbox";
-import Toast from "react-native-toast-message";
 import type { Order } from "@/types/orders";
 
-type OrderCardCompleteCheckboxProps = {
-  order: Order;
-  setOrder: (order: ((prevState: Order) => Order) | Order) => void;
-};
-
-export default function OrderCardIssuedCheckbox({
-  order,
-  setOrder,
-}: OrderCardCompleteCheckboxProps) {
-  const { user, showAlert } = useGlobalContext();
-  const { theme, colors } = useThemeContext();
+function OrderCardIssuedCheckbox({ order }: { order: Order }) {
+  const { user } = useGlobalContext();
+  const { colors } = useThemeContext();
+  const changeCompletedStatus = useChangeCheckboxValue(order, "issued");
 
   const userHasAccess = user?.role === "admin" || user?.role === "moderator";
-
-  // Toggle the completed status of the passed order
-  const changeCompletedStatus = useCallback(
-    async (order: Order) => {
-      // If the order is completed and the user doesn't have access, return
-      if (!userHasAccess || order.completed) return;
-
-      try {
-        const updatedOrder = {
-          userId: order.user.$id,
-          buyerId: order.buyer.$id,
-          quantity: order.quantity,
-          completed: order.completed,
-          additionalInfo: order.additionalInfo,
-          issued: !order.issued,
-          deliveryDate: order.deliveryDate,
-        };
-
-        // Update the order in the local state for immediate feedback to the user
-        setOrder((prevOrder) => ({ ...prevOrder, issued: !prevOrder.issued }));
-
-        // Update the order in the database
-        await editOrder(order.$id, updatedOrder);
-
-        // Show a success message to the user
-        Toast.show({
-          type: theme === "light" ? "success" : "successDark",
-          text1: "Zmieniono status zamówienia na",
-          text2: `${order.issued === false ? "skompletowane" : "nieskompletowane"}.`,
-          text2Style: { fontWeight: "bold" },
-        });
-      } catch (error) {
-        // Show an alert to the user if the operation failed
-        showAlert("Błąd", "Nie udało się zaktualizować zamówienia.");
-      }
-    },
-    [order, editOrder]
-  );
 
   return (
     <Checkbox
@@ -64,7 +18,14 @@ export default function OrderCardIssuedCheckbox({
       status={order.issued}
       fillColor={colors.green}
       disabled={!userHasAccess || order.completed}
-      onPress={() => changeCompletedStatus(order)}
+      onPress={() => {
+        // If the order is completed or the user doesn't have access, return
+        if (!userHasAccess || order.completed) return;
+
+        changeCompletedStatus();
+      }}
     />
   );
 }
+
+export default memo(OrderCardIssuedCheckbox);
