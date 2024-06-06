22c3028dd95e83bf03dedee6112463154403484b
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-06-06 15:46:02 +0200

    Moved logic into `useEditUser` hook.

--- components/Settings/SettingsEditSection.tsx
+++ components/Settings/SettingsEditSection.tsx
@@ -1,15 +1,10 @@
 import { View } from "react-native";
-import React, { useCallback, useState } from "react";
 import { useGlobalContext } from "@/hooks/useGlobalContext";
-import { useThemeContext } from "@/hooks/useThemeContext";
-import { useOrdersContext } from "@/hooks/useOrdersContext";
-import { useModalContext } from "@/hooks/useModalContext";
+import { useEditUser } from "@/hooks/SettingsHooks/useEditUser";
 import tw from "@/lib/twrnc";
-import { editUserEmail, editUserUsername, editUserPassword } from "@/api/appwrite/users";
 import SettingsEditSectionWrapper from "./SettingsEditSectionWrapper";
 import SettingsSection from "./SettingsSection";
 import { FormField } from "../FormField";
-import Toast from "react-native-toast-message";
 import type { EditSettingsOptions } from "@/types/editSettingsOptions";
 
 type SettingsEditSectionProps = {
@@ -27,146 +22,44 @@ export default function SettingsEditSection({
   isSubmitting,
   setIsSubmitting,
 }: SettingsEditSectionProps) {
-  const { user, refetchUser } = useGlobalContext();
-  const { theme } = useThemeContext();
-  const { ordersData } = useOrdersContext();
-  const { showModal, setModalData } = useModalContext();
+  const { user } = useGlobalContext();
 
-  const [modalInputValue, setModalInputValue] = useState("");
-  const [modalInput2Value, setModalInput2Value] = useState(""); // Only for case when password is being changed
-  const [passwordConfirmationInputValue, setPasswordConfirmationInputValue] = useState("");
-
-  const initialErrorsState: Record<EditSettingsOptions | "passwordConfirmation", string[] | null> =
-    {
-      email: null,
-      username: null,
-      password: null,
-      passwordConfirmation: null,
-    };
-  const [errors, setErrors] = useState<typeof initialErrorsState>(initialErrorsState);
+  const {
+    editUserData,
+    errors,
+    modalInput2Value,
+    modalInputValue,
+    passwordConfirmationInputValue,
+    setModalInput2Value,
+    setModalInputValue,
+    setPasswordConfirmationInputValue,
+    resetStates,
+  } = useEditUser(type, setIsSubmitting, inputVisible, setInputVisible);
 
   // Set form label and placeholder based on the type
   let formLabel: string;
   let formPlaceholder: string;
-  let errorTheSameValue: string;
-  let editError: string;
   switch (type) {
     case "email":
       formLabel = "Email:";
       formPlaceholder = "Wprowadź nowy email";
-      errorTheSameValue = "Email nie został zmieniony";
-      editError = "Nie udało się zaktualizować email.";
       break;
 
     case "username":
       formLabel = "Nazwa użytkownika:";
       formPlaceholder = "Wprowadź nową nazwę";
-      errorTheSameValue = "Nazwa użytkownika nie została zmieniona";
-      editError = "Nie udało się zaktualizować nazwy użytkownika.";
       break;
 
     case "password":
       formLabel = inputVisible === type ? "Nowe hasło:" : "Hasło:";
       formPlaceholder = "Wprowadź nowe hasło";
-      editError = "Nie udało się zaktualizować hasła.";
       break;
   }
-
-  // Function to edit user data
-  const editUserData = useCallback(async () => {
-    if (!user) return;
-
-    // Reset all errors
-    setErrors(initialErrorsState);
-
-    // Check if the input value has been changed, if not then show an error
-    if (type !== "password" && modalInputValue === user[type]) {
-      setErrors((prevErrors) => ({
-        ...prevErrors,
-        [type]: [errorTheSameValue],
-      }));
-      return;
-    }
-
-    // If type is not "username" then check if the password confirmation input value is empty, if so then show an error
-    if (
-      type !== "username" &&
-      (!passwordConfirmationInputValue || passwordConfirmationInputValue === "")
-    ) {
-      setErrors((prevErrors) => ({
-        ...prevErrors,
-        passwordConfirmation: ["Pole wymagane"],
-      }));
-      return;
-    }
-
-    try {
-      setIsSubmitting(true);
-
-      // Call the appropriate function based on the type
-      const editErrors =
-        type === "email" // If type is "email" then call editUserEmail function
-          ? await editUserEmail(user.$id, modalInputValue, passwordConfirmationInputValue)
-          : type === "username" // If type is "username" then call editUserUsername function
-          ? await editUserUsername(user.$id, modalInputValue)
-          : // Otherwise the type has to be "password" so call editUserPassword function
-            await editUserPassword(modalInputValue, passwordConfirmationInputValue);
-
-      // If there are any errors then show them
-      if (editErrors) {
-        setErrors((prevErrors) => ({ ...prevErrors, [type]: editErrors }));
-      }
-      // Else show a success message, reset the states and update the user data
-      else {
-        Toast.show({
-          type: theme === "light" ? "success" : "successDark",
-          text1: "Dane zostały zmienione.",
-          topOffset: 50,
-          text1Style: { textAlign: "center" },
-        });
-        resetStates();
-        refetchUser();
-        // Refetch the orders data only if the type is "username" because the username is used in the orders data
-        type === "username" && ordersData?.refetchData();
-      }
-    } catch (error) {
-      // If there was an error then show a modal with an error message
-      setModalData({
-        title: editError,
-        subtitle:
-          "Wystąpił błąd podczas próby aktualizacji danych konta. Sprawdź poprawność wprowadzonego hasła i spróbuj ponownie.",
-        btn1: { text: "Ok" },
-      });
-      showModal();
-    } finally {
-      setIsSubmitting(false);
-    }
-  }, [
-    user,
-    type,
-    modalInputValue,
-    passwordConfirmationInputValue,
-    setIsSubmitting,
-    setErrors,
-    setModalData,
-    showModal,
-    resetStates,
-    refetchUser,
-    ordersData,
-  ]);
 
   function openUserDataEdit() {
     setInputVisible(type);
     // If the type is not "password" then set the modal input value to the user data based on the type
     type !== "password" && setModalInputValue(user![type]);
-  }
-
-  function resetStates() {
-    setInputVisible(false);
-    setModalInputValue("");
-    setModalInput2Value("");
-    setPasswordConfirmationInputValue("");
-    setErrors(initialErrorsState);
   }
 
   return (
@@ -190,6 +83,7 @@ export default function SettingsEditSection({
               title={formLabel}
               placeholder={formPlaceholder}
               titleStyles="font-poppinsSemiBold text-sm"
+              secureTextEntry={type === "password"}
               value={modalInputValue}
               handleChangeText={(e) => setModalInputValue(e)}
               errors={errors[type]}
