c02ccacac42074dd8a8c34eee3720ff1d04c161d
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-06-06 15:36:53 +0200

    Moved logic related to changing current price to `useCurrentPrice` hook.

--- components/CurrentPrice.tsx
+++ components/CurrentPrice.tsx
@@ -1,24 +1,17 @@
 import { View } from "react-native";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useGlobalContext } from "@/hooks/useGlobalContext";
-import { useThemeContext } from "@/hooks/useThemeContext";
-import { useModalContext } from "@/hooks/useModalContext";
 import { useOrdersContext } from "@/hooks/useOrdersContext";
 import { useAppwrite } from "@/hooks/useAppwrite";
-import { updatePrice as appwriteUpdatePrice, getCurrentPrice } from "@/api/appwrite/currentPrice";
+import { useCurrentPrice } from "@/hooks/useCurrentPrice";
+import { getCurrentPrice } from "@/api/appwrite/currentPrice";
 import tw from "@/lib/twrnc";
-import Toast from "react-native-toast-message";
 import PriceLoadingText from "./Settings/PriceLoadingText";
 import CustomButton from "./CustomButton";
 
 export default function CurrentPrice() {
-  const { user, showAlert } = useGlobalContext();
-  const { theme, colors } = useThemeContext();
-  const { showModal, setModalData } = useModalContext();
+  const { user } = useGlobalContext();
   const { currentPrice, setCurrentPrice, ordersData } = useOrdersContext();
-
-  // Create local state for modal input value
-  const [modalInputValue, setModalInputValue] = useState(currentPrice?.price.toString() || "");
 
   const userHasAccess = user?.role === "admin" || user?.role === "moderator";
 
@@ -38,83 +31,13 @@ export default function CurrentPrice() {
     message: "Nie udało się pobrać aktualnej ceny.",
   });
 
-  const openModal = useCallback(() => {
-    if (!currentPrice) return;
+  const { changeCurrentPrice, isSubmitting } = useCurrentPrice(refetchPrice);
 
-    setModalData({
-      title: "Wprowadź nową cenę",
-      btn1: {
-        text: "Anuluj",
-      },
-      btn2: {
-        text: "Zapisz",
-        color: "primary",
-      },
-      input: {
-        maxLength: 4,
-        keyboardType: "number-pad",
-        defaultValue: modalInputValue,
-        onChangeText: (text) => setModalInputValue(text),
-      },
-    });
-    showModal();
-  }, [currentPrice, showModal, setModalData, modalInputValue, setModalInputValue]);
-
-  const updatePrice = useCallback(async () => {
-    if (!userHasAccess) return; // If user doesn't have access, return
-
-    if (!currentPrice) return; // If currentPrice is null, return
-
-    if (modalInputValue === "") {
-      // If modalInputValue is empty, return alert
-      return showAlert("Błąd", "Wprowadź poprawną cenę.");
-    }
-    if (modalInputValue === currentPrice.price?.toString()) {
-      // If price doesn't change, do nothing
-      return;
-    }
-
-    try {
-      const updatedPrice = await appwriteUpdatePrice(modalInputValue, currentPrice.$id);
-
-      // If there are errors, show alert and return
-      if (updatedPrice.errors) {
-        return showAlert("Nieprawidłowa wartość", `${updatedPrice.errors?.join("\n")}`);
-      }
-
-      // else show success toast and refetch price
-      Toast.show({
-        type: theme === "light" ? "success" : "successDark",
-        text1: "Cena została zaktualizowana.",
-      });
-      refetchPrice();
-    } catch (error: any) {
-      setModalData({
-        title: "Błąd",
-        subtitle: "Nie udało się zaktualizować ceny.",
-        btn1: { text: "Ok" },
-      });
-      showModal();
-    }
-  }, [modalInputValue, currentPrice]);
-
-  // Update the updatePrice function in the modal data when the input value changes
+  // Update the current price when the fetchedCurrentPrice is defined
   useEffect(() => {
-    setModalData((prevModalData) => ({
-      ...prevModalData,
-      btn2: {
-        ...prevModalData.btn2,
-        onPress: updatePrice,
-      },
-    }));
-  }, [modalInputValue]);
-
-  // Update the current price and set modal input default value when the fetchedCurrentPrice is defined
-  useEffect(() => {
-    if (!fetchedCurrentPrice) return; // Wait for fetchedCurrentPrice to be defined
+    if (!fetchedCurrentPrice) return;
 
     setCurrentPrice(fetchedCurrentPrice);
-    setModalInputValue(fetchedCurrentPrice.price.toString());
   }, [fetchedCurrentPrice]);
 
   return (
@@ -130,8 +53,11 @@ export default function CurrentPrice() {
           <CustomButton
             text="Zmień cenę"
             textStyles="text-xs p-3"
-            onPress={openModal}
-            disabled={currentPrice === null || isPriceLoading}
+            containerStyles="w-[95px] h-[40px]"
+            onPress={changeCurrentPrice}
+            disabled={currentPrice === null || isSubmitting}
+            loading={isSubmitting}
+            loadingSpinnerSize="small"
           />
         )}
       </View>
