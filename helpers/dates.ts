9881847af7d151ffef4b6a2aeceddf1a1714c25b
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-06-13 13:37:44 +0200

    Edited function type.

--- helpers/dates.ts
+++ helpers/dates.ts
@@ -1,6 +1,6 @@
 import { format } from "date-fns/format";
 import { pl } from "date-fns/locale/pl";
 
-export function formatDate(date: string, formatStr?: string) {
+export function formatDate(date: string | number | Date, formatStr?: string) {
   return format(date, formatStr ? formatStr : "dd.MM.yyyy", { locale: pl });
 }
