1236b16f39a56b1ece98299e4d7d2de7e168ceac
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-06-13 08:49:22 +0200

    Added function to get weather forecast.

--- api/weatherStation/weather.ts
+++ api/weatherStation/weather.ts
@@ -6,9 +6,6 @@ export async function getCurrentWeatherData() {
     const response = await axios.get(
       `${baseWeatherApiUrl}/real_time?application_key=${weatherConfig.appKey}&api_key=${weatherConfig.apiKey}&mac=${weatherConfig.weatherStationMacAddress}&${callBackOptionsUrl}`
     );
-    console.log(
-      `${baseWeatherApiUrl}/real_time?application_key=${weatherConfig.appKey}&api_key=${weatherConfig.apiKey}&mac=${weatherConfig.weatherStationMacAddress}&${callBackOptionsUrl}`
-    );
 
     return response.data.data;
   } catch (error) {
@@ -16,7 +13,7 @@ export async function getCurrentWeatherData() {
   }
 }
 
-export async function getWeatherDataByDate(startDate: string, endDate: string) {
+export async function getWeatherDataHistoryByDate(startDate: string, endDate: string) {
   try {
     const response = await axios.get(
       `${baseWeatherApiUrl}/history?application_key=${weatherConfig.appKey}&api_key=${weatherConfig.apiKey}&mac=${weatherConfig.weatherStationMacAddress}&start_date=${startDate} 00:00:00&end_date=${endDate} 23:59:59&cycle_type=auto&${callBackOptionsUrl}`
@@ -24,6 +21,18 @@ export async function getWeatherDataByDate(startDate: string, endDate: string) {
 
     return response.data.data;
   } catch (error) {
-    throw new Error("Nie udało się pobrać aktualnej pogody");
+    throw new Error("Nie udało się pobrać historii pogody");
+  }
+}
+
+export async function getForecast() {
+  try {
+    const response = await axios.get(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Jaczew?unitGroup=us&include=fcst%2Cevents%2Cdays%2Ccurrent%2Chours%2Calerts&key=${weatherConfig.forecastApiKey}&contentType=json`
+    );
+
+    return response.data;
+  } catch (error) {
+    throw new Error("Nie udało się pobrać prognozy pogody");
   }
 }
