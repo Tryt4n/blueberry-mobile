86c71ffe1fa8f8cf3431d933ea8cbe3bf1a014ed
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-06-14 10:07:28 +0200

    Added types for returned values.

--- api/weatherStation/weather.ts
+++ api/weatherStation/weather.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { baseWeatherApiUrl, weatherConfig, callBackOptionsUrl } from "./weatherAPIConfig";
+import type { CurrentWeather, ForecastWeather, HistoryWeather } from "@/types/weather";
 
 export async function getCurrentWeatherData() {
   try {
@@ -7,7 +8,7 @@ export async function getCurrentWeatherData() {
       `${baseWeatherApiUrl}/real_time?application_key=${weatherConfig.appKey}&api_key=${weatherConfig.apiKey}&mac=${weatherConfig.weatherStationMacAddress}&${callBackOptionsUrl}`
     );
 
-    return response.data.data;
+    return response.data.data as CurrentWeather;
   } catch (error) {
     throw new Error("Nie udało się pobrać aktualnej pogody");
   }
@@ -19,7 +20,7 @@ export async function getWeatherDataHistoryByDate(startDate: string, endDate: st
       `${baseWeatherApiUrl}/history?application_key=${weatherConfig.appKey}&api_key=${weatherConfig.apiKey}&mac=${weatherConfig.weatherStationMacAddress}&start_date=${startDate} 00:00:00&end_date=${endDate} 23:59:59&cycle_type=auto&${callBackOptionsUrl}`
     );
 
-    return response.data.data;
+    return response.data.data as HistoryWeather;
   } catch (error) {
     throw new Error("Nie udało się pobrać historii pogody");
   }
@@ -28,10 +29,10 @@ export async function getWeatherDataHistoryByDate(startDate: string, endDate: st
 export async function getForecast() {
   try {
     const response = await axios.get(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Jaczew?unitGroup=us&include=fcst%2Cevents%2Cdays%2Ccurrent%2Chours%2Calerts&key=${weatherConfig.forecastApiKey}&contentType=json`
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Jaczew?unitGroup=metric&include=fcst%2Cevents%2Cdays%2Ccurrent%2Chours%2Calerts&key=${weatherConfig.forecastApiKey}&lang=pl&contentType=json`
     );
 
-    return response.data;
+    return response.data as ForecastWeather;
   } catch (error) {
     throw new Error("Nie udało się pobrać prognozy pogody");
   }
