fc1b09e5df0c48539c90831fb610b2d25123eafb
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-06-18 15:15:53 +0200

    Created helper functions to map days/hours and return only necessary values.

--- helpers/weather.ts
+++ helpers/weather.ts
@@ -1,3 +1,5 @@
+import type { ForecastWeather } from "@/types/weather";
+
 export function findExtremeValue(valuesList: Record<string, string>, find: "min" | "max") {
   const extremeValue =
     find === "max"
@@ -9,4 +11,63 @@ export function findExtremeValue(valuesList: Record<string, string>, find: "min"
   )?.[0];
 
   return { extremeValue, extremeValueTime };
+}
+function mapHours(hour: ForecastWeather["days"][number]["hours"][number]) {
+  const {
+    datetime,
+    datetimeEpoch,
+    temp,
+    icon,
+    conditions,
+    windspeed,
+    winddir,
+    precip,
+    preciptype,
+    precipprob,
+  } = hour;
+
+  return {
+    datetime,
+    datetimeEpoch,
+    temp,
+    icon,
+    conditions,
+    windspeed,
+    winddir,
+    precip,
+    preciptype,
+    precipprob,
+  };
+}
+
+export function mapDays(day: ForecastWeather["days"][number], index: number) {
+  const {
+    datetime,
+    datetimeEpoch,
+    tempmax,
+    tempmin,
+    icon,
+    conditions,
+    windspeed,
+    winddir,
+    precip,
+    preciptype,
+    precipprob,
+    hours,
+  } = day;
+
+  return {
+    datetime,
+    datetimeEpoch,
+    tempmax,
+    tempmin,
+    icon,
+    conditions,
+    windspeed,
+    winddir,
+    precip,
+    preciptype,
+    precipprob,
+    hours: index < 3 ? hours.map(mapHours) : undefined,
+  };
 }
