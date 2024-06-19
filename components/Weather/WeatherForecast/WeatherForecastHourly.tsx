aee1ce25498b3eacb130acd5b7a022dd7b620375
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-06-19 08:35:39 +0200

    Changed to use `<WeatherForecastListItem/>` component.

--- components/Weather/WeatherForecast/WeatherForecastHourly.tsx
+++ components/Weather/WeatherForecast/WeatherForecastHourly.tsx
@@ -1,25 +1,18 @@
-import { View, Text, Image } from "react-native";
+import { View, Text } from "react-native";
 import { useThemeContext } from "@/hooks/useThemeContext";
 import tw from "@/lib/twrnc";
 import { isAfter, isThisHour, isToday, startOfHour } from "date-fns";
 import { formatDate } from "@/helpers/dates";
-import { weatherIcons } from "@/constants/weatherIcons";
-import WeatherForecastLayout from "./WeatherForecastLayout";
-import { Ionicons } from "@expo/vector-icons";
 import { FlashList } from "@shopify/flash-list";
+import WeatherForecastLayout from "./WeatherForecastLayout";
+import WeatherForecastListItem from "./WeatherForecastListItem";
 import type { WeatherShortenedForecast } from "./WeatherForecast";
-
-type WeatherForecastHourlyProps = {
-  forecast: WeatherShortenedForecast;
-  tempUnit: string;
-  windUnit: string;
-};
 
 export default function WeatherForecastHourly({
   forecast,
-  tempUnit,
-  windUnit,
-}: WeatherForecastHourlyProps) {
+}: {
+  forecast: WeatherShortenedForecast;
+}) {
   const { colors } = useThemeContext();
 
   return (
@@ -29,7 +22,7 @@ export default function WeatherForecastHourly({
           key={day.datetimeEpoch}
           style={tw`${dayIndex === 0 ? "ml-0" : "mx-4"}`}
         >
-          <Text style={tw`font-poppinsRegular text-[${colors.text}] uppercase`}>
+          <Text style={tw`mb-1 font-poppinsMedium text-base text-[${colors.text}] uppercase`}>
             {isToday(day.datetime) ? "Dzisiaj" : formatDate(day.datetime, "EE dd")}
           </Text>
 
@@ -48,94 +41,21 @@ export default function WeatherForecastHourly({
                 keyExtractor={(item) => item.datetimeEpoch.toString()}
                 estimatedItemSize={day.hours.length}
                 renderItem={({ item: hour, index: hourIndex }) => (
-                  <View
-                    key={hour.datetimeEpoch}
-                    style={tw`${hourIndex === 0 ? "ml-0 mr-4" : "mx-4"} items-center flex-1`}
-                  >
-                    <Text style={tw`font-poppinsRegular text-[${colors.text}] uppercase`}>
-                      {isThisHour(hour.datetimeEpoch * 1000)
+                  <WeatherForecastListItem
+                    data={
+                      {
+                        ...hour,
+                        tempmax: hour.temp,
+                        tempmin: hour.temp,
+                      } as WeatherShortenedForecast[number]
+                    }
+                    index={hourIndex}
+                    currentText={
+                      isThisHour(hour.datetimeEpoch * 1000)
                         ? "Teraz"
-                        : formatDate(hour.datetimeEpoch * 1000, "HH")}
-                    </Text>
-
-                    <View style={tw`mt-1 flex-row items-baseline`}>
-                      <View style={tw`flex-row items-baseline`}>
-                        <Text style={tw`font-poppinsSemiBold text-base text-[${colors.text}]`}>
-                          {hour.temp}
-                        </Text>
-                        <Text style={tw`font-poppinsRegular text-base text-[${colors.textAccent}]`}>
-                          &nbsp;{tempUnit}
-                        </Text>
-                      </View>
-                    </View>
-
-                    <View style={tw`items-center max-w-[155px]`}>
-                      <Image source={weatherIcons[hour.icon as keyof typeof weatherIcons]} />
-                      <Text
-                        style={tw`font-poppinsRegular text-xs text-[${colors.textAccent}] text-center`}
-                      >
-                        {hour.conditions}
-                      </Text>
-                    </View>
-
-                    <View style={tw`mt-1 flex-row items-center gap-x-1`}>
-                      <Ionicons
-                        name="arrow-up-circle-outline"
-                        size={20}
-                        color={colors.textAccent}
-                        style={{ transform: [{ rotate: `${hour.winddir}deg` }] }} // rotate arrow icon based on wind direction
-                      />
-                      <Text style={tw`font-poppinsRegular text-[${colors.textAccent}] text-center`}>
-                        {hour.windspeed}
-                        {windUnit}
-                      </Text>
-                    </View>
-
-                    <View style={tw`mt-1 items-center gap-x-1`}>
-                      <View style={tw`flex-row items-center`}>
-                        <View style={tw`flex-row items-center`}>
-                          <Ionicons
-                            name={
-                              !hour.precip || hour.preciptype?.includes("rain") ? "water" : "snow"
-                            } // if no precip or rain, show water icon
-                            size={14}
-                            color={colors.primary}
-                          />
-                          {
-                            // if precip type is snow, ice or freezing rain, show additional snow icon
-                            hour.preciptype?.map((type) => {
-                              if (type === "snow" || type === "ice" || type === "freezingrain")
-                                return (
-                                  <>
-                                    <Text style={tw`font-poppinsRegular text-[${colors.primary}]`}>
-                                      /
-                                    </Text>
-                                    <Ionicons
-                                      name="snow"
-                                      size={14}
-                                      color={colors.primary}
-                                    />
-                                  </>
-                                );
-                            })
-                          }
-                        </View>
-
-                        <Text style={tw`font-poppinsRegular text-[${colors.primary}]`}>
-                          {hour.precipprob?.toFixed()}%
-                        </Text>
-                      </View>
-
-                      {hour.precipprob &&
-                      hour.precipprob >= 50 &&
-                      hour.precip &&
-                      hour.precip > 0 ? (
-                        <Text style={tw`font-poppinsRegular text-[${colors.primary}]`}>
-                          {hour.precip.toFixed(1)}mm
-                        </Text>
-                      ) : null}
-                    </View>
-                  </View>
+                        : formatDate(hour.datetimeEpoch * 1000, "HH")
+                    }
+                  />
                 )}
               />
             </View>
