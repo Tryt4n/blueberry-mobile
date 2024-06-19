48f2744b5bc053c238ad866ca1fdcca59c62e6c7
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-06-19 09:30:07 +0200

    Created scroll to top component.

---
+++ components/ScrollToTop.tsx
@@ -0,0 +1,22 @@
+import { TouchableOpacity } from "react-native";
+import { useThemeContext } from "@/hooks/useThemeContext";
+import tw from "@/lib/twrnc";
+import { Ionicons } from "@expo/vector-icons";
+import type { ComponentProps } from "react";
+
+export default function ScrollToTop({ ...props }: ComponentProps<typeof TouchableOpacity>) {
+  const { colors } = useThemeContext();
+
+  return (
+    <TouchableOpacity
+      style={tw`absolute right-[6.5%] bottom-[5%] rounded-full p-2 bg-[${colors.primary}] cursor-pointer`}
+      {...props}
+    >
+      <Ionicons
+        name="arrow-up"
+        size={24}
+        color={colors.text}
+      />
+    </TouchableOpacity>
+  );
+}
