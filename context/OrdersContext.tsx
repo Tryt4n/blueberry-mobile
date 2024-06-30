22ce15bb2947e9d5210f7287df0bd410588a3b3b
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-06-30 17:43:08 +0200

    Moved fetching buyers from `useOrder` hook.

--- context/OrdersContext.tsx
+++ context/OrdersContext.tsx
@@ -1,28 +1,34 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { useGlobalContext } from "@/hooks/useGlobalContext";
+import { useDataFetch } from "@/hooks/useDataFetch";
+import { getOrders } from "@/api/appwrite/orders";
+import { getBuyers } from "@/api/appwrite/buyers";
 import { format } from "date-fns";
 import { pl } from "date-fns/locale/pl";
 import type { Order, OrdersDataType, OrdersSearchParams } from "@/types/orders";
 import type { CurrentPrice } from "@/types/currentPrice";
+import type { Buyer } from "@/types/buyers";
 
 type OrderContextType = {
   currentPrice: CurrentPrice | null;
   setCurrentPrice: (obj: CurrentPrice) => void;
   ordersData: OrdersDataType | null;
-  setOrdersData: (obj: OrdersDataType | null) => void;
   editedOrder: Order | null;
   setEditedOrder: (order: Order | null) => void;
   isBannerVisible: boolean;
   setIsBannerVisible: (value: boolean) => void;
+  fetchedBuyers: ReturnType<typeof useDataFetch> & { data?: Buyer[] };
   ordersSearchParams: OrdersSearchParams;
   setOrdersSearchParams: (
     value: ((prevState: OrdersSearchParams) => OrdersSearchParams) | OrdersSearchParams
   ) => void;
   today: string;
 };
-
 export const OrdersContext = createContext<OrderContextType | null>(null);
 
 export default function OrderContextProvider({ children }: { children: React.ReactNode }) {
+  const { user } = useGlobalContext();
+
   const [ordersData, setOrdersData] = useState<OrderContextType["ordersData"]>(null);
   const [editedOrder, setEditedOrder] = useState<Order | null>(null);
   const [currentPrice, setCurrentPrice] = useState<CurrentPrice | null>(null);
@@ -35,15 +41,54 @@ export default function OrderContextProvider({ children }: { children: React.Rea
     userId: undefined,
   });
 
+  const errorMessage = {
+    title: "Błąd",
+    message: "Nie udało się pobrać zamówień. Spróbuj odświeżyć stronę.",
+  };
+
+  const appwriteOrdersData = useDataFetch(
+    getOrders,
+    [
+      ordersSearchParams.startDate,
+      ordersSearchParams.endDate,
+      user && user.role !== "admin" && user.role !== "moderator"
+        ? user.$id
+        : ordersSearchParams.userId
+        ? ordersSearchParams.userId
+        : undefined,
+    ],
+    errorMessage
+  );
+
+  // Refetch orders data when search params change
+  useEffect(() => {
+    appwriteOrdersData.refetchData();
+  }, [ordersSearchParams]);
+
+  // Set orders data to the context state if the data is loaded successfully
+  useEffect(() => {
+    setOrdersData(appwriteOrdersData);
+  }, [appwriteOrdersData.isLoading, ordersSearchParams]);
+
+  // Fetch all buyers
+  const fetchedBuyers = useDataFetch(
+    getBuyers,
+    user?.role === "admin" || user?.role === "moderator" ? [] : [user?.$id],
+    {
+      title: "Błąd",
+      message: "Nie udało się pobrać klientów. Spróbuj odświeżyć stronę.",
+    }
+  );
+
   const contextValues: OrderContextType = {
     currentPrice,
     setCurrentPrice,
     ordersData,
-    setOrdersData,
     editedOrder,
     setEditedOrder,
     isBannerVisible,
     setIsBannerVisible,
+    fetchedBuyers,
     ordersSearchParams,
     setOrdersSearchParams,
     today: formattedTodayDate,
