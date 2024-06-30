f9d22f798e9375739c123ed1c6004e1fed75b299
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-06-30 17:36:07 +0200

    Moved all logic into newly created `<Orders/>` component and wrapped it into orders context.

--- app/(drawer)/(tabs)/orders.tsx
+++ app/(drawer)/(tabs)/orders.tsx
@@ -1,79 +1,10 @@
-import { View, Text, ActivityIndicator } from "react-native";
-import React, { useEffect } from "react";
-import { useGlobalContext } from "@/hooks/useGlobalContext";
-import { useThemeContext } from "@/hooks/useThemeContext";
-import { useOrdersContext } from "@/hooks/useOrdersContext";
-import { useDataFetch } from "@/hooks/useDataFetch";
-import { getOrders } from "@/api/appwrite/orders";
-import tw from "@/lib/twrnc";
-import PageLayout from "@/layout/PageLayout";
-import OrdersList from "@/components/Orders/OrdersList";
-import OrdersHeader from "@/components/Orders/OrdersHeader";
-import OrderBottomSheet from "@/components/OrderBottomSheet/OrderBottomSheet";
+import OrdersLayout from "@/layout/OrdersLayout";
+import Orders from "@/components/Orders/Orders";
 
 export default function TabOrders() {
-  const { user } = useGlobalContext();
-  const { colors } = useThemeContext();
-  const { ordersData, setOrdersData, ordersSearchParams } = useOrdersContext();
-
-  const errorMessage = {
-    title: "Błąd",
-    message: "Nie udało się pobrać zamówień. Spróbuj odświeżyć stronę.",
-  };
-
-  const appwriteOrdersData = useDataFetch(
-    getOrders,
-    [
-      ordersSearchParams.startDate,
-      ordersSearchParams.endDate,
-      user && user.role !== "admin" && user.role !== "moderator"
-        ? user.$id
-        : ordersSearchParams.userId
-        ? ordersSearchParams.userId
-        : undefined,
-    ],
-    errorMessage
-  );
-
-  // Refetch orders data when search params change
-  useEffect(() => {
-    appwriteOrdersData.refetchData();
-  }, [ordersSearchParams]);
-
-  // Set orders data to the context state if the data is loaded successfully
-  useEffect(() => {
-    setOrdersData(appwriteOrdersData);
-  }, [appwriteOrdersData.isLoading, ordersSearchParams]);
-
   return (
-    <PageLayout>
-      {user && ordersData && (
-        <>
-          <View style={tw`h-full`}>
-            <OrdersHeader />
-
-            {ordersData.isLoading ? (
-              <ActivityIndicator
-                size="large"
-                color={colors.primary}
-                style={tw`my-8`}
-              />
-            ) : (
-              <>
-                {ordersData.data && ordersData.data.length > 0 ? (
-                  <OrdersList />
-                ) : (
-                  <Text style={tw`text-center font-poppinsRegular text-lg text-[${colors.text}]`}>
-                    Brak zamówień
-                  </Text>
-                )}
-              </>
-            )}
-          </View>
-        </>
-      )}
-
-      <OrderBottomSheet />
-    </PageLayout>
+    <OrdersLayout>
+      <Orders />
+    </OrdersLayout>
   );
 }
