6be0ea3dbc045d3dc8a896187d8a256d3e31a67e
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-06-30 17:39:34 +0200

    Created setting component to change user view preferences.

---
+++ components/Settings/SettingsSimplifiedView.tsx
@@ -0,0 +1,65 @@
+import { View, Text, ActivityIndicator } from "react-native";
+import { useState } from "react";
+import { useGlobalContext } from "@/hooks/useGlobalContext";
+import { useThemeContext } from "@/hooks/useThemeContext";
+import { editUserViewPreferences } from "@/api/appwrite/users";
+import tw from "@/lib/twrnc";
+import { Checkbox } from "react-native-paper";
+import Toast from "react-native-toast-message";
+
+export default function SettingsSimplifiedView() {
+  const { user, isSimplifiedView, setIsSimplifiedView, refetchUser, showAlert } =
+    useGlobalContext();
+  const { theme, colors } = useThemeContext();
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function changeUserViewPreferences() {
+    if (!user) return;
+
+    setIsLoading(true);
+
+    try {
+      await editUserViewPreferences(user.$id, !isSimplifiedView).then(() => {
+        setIsSimplifiedView(!isSimplifiedView);
+        refetchUser();
+        Toast.show({
+          type: theme === "light" ? "success" : "successDark",
+          text1: "Pomyślnie zmieniono",
+          text2: "preferencje wyświetlania.",
+          topOffset: 50,
+        });
+      });
+    } catch (error) {
+      showAlert("Błąd", "Wystąpił błąd podczas zmiany preferencji widoku. Spróbuj ponownie.");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <View style={tw`flex-row gap-x-2 items-center justify-center`}>
+      <Text style={tw`font-poppinsSemiBold text-[${colors.text}]`}>Widok uproszczony</Text>
+
+      {isLoading ? (
+        <ActivityIndicator
+          size={"small"}
+          color={colors.primary}
+        />
+      ) : (
+        <Checkbox
+          status={
+            isSimplifiedView === true
+              ? "checked"
+              : isSimplifiedView === false
+              ? "unchecked"
+              : "indeterminate"
+          }
+          color={colors.primary}
+          uncheckedColor={colors.placeholder}
+          disabled={isSimplifiedView === undefined || isLoading}
+          onPress={changeUserViewPreferences}
+        />
+      )}
+    </View>
+  );
+}
