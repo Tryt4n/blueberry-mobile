9a1f5178dbda4bae70db7c10e5c922ab2e6b2a15
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-07-02 10:39:36 +0200

    Changed to use `<SimplifiedInput/>` component.

--- components/Modal/ModalInput.tsx
+++ components/Modal/ModalInput.tsx
@@ -1,28 +1,14 @@
-import { View, TextInput } from "react-native";
-import { useState, type ComponentProps } from "react";
-import { useGlobalContext } from "@/hooks/useGlobalContext";
-import { useThemeContext } from "@/hooks/useThemeContext";
+import { TextInput } from "react-native";
 import tw from "@/lib/twrnc";
+import SimplifiedInput from "../SimplifiedInput";
+import type { ComponentProps } from "react";
 
 export default function ModalInput({ ...props }: ComponentProps<typeof TextInput>) {
-  const { platform } = useGlobalContext();
-  const { colors } = useThemeContext();
-  const [isFocused, setIsFocused] = useState(false);
-
   return (
-    <View style={tw`flex justify-center items-center mt-2`}>
-      <TextInput
-        style={[
-          tw`w-32 border-b-2 text-center font-poppinsMedium text-3xl text-[${colors.text}]${
-            isFocused ? ` border-[${colors.primary}]` : ` border-[${colors.inputBorder}]`
-          }`,
-          platform === "web" ? { outline: "none", caretColor: colors.primary } : {},
-        ]}
-        cursorColor={colors.primary}
-        onFocus={() => setIsFocused(true)}
-        onBlur={() => setIsFocused(false)}
-        {...props}
-      />
-    </View>
+    <SimplifiedInput
+      containerStyles={tw`flex justify-center items-center mt-2`}
+      inputStyles="w-32 text-3xl"
+      {...props}
+    />
   );
 }
